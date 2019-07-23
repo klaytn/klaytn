8dc0aceda63b8ab1252f05d71c1857474138d15e
laggu <sebastian.ra@groundx.xyz>
2019-07-23 17:04:35 +0900

    reflect comment

--- reward/doc.go
+++ reward/doc.go
@@ -15,10 +15,8 @@
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
 /*
-Package Reward
-
 Package Reward implements the Klaytn Reward System.
-The Klaytn Reward System manages stakingInfo and distributes block reward.
+The Klaytn Reward System manages stakingInfo and distributes block rewards.
 
 Managing stakingInfo
 
@@ -31,23 +29,23 @@ StakingAmount is a balance how much KLAY each node has staked. Only CCO can stak
 Each CCO stakes KLAY by a smart contract called staking contract.
 StakingAmount is derived by checking balance of staking contract.
 A StakingInfo has 5 types of addresses. All addresses are obtained by the addressBookContract which is pre-deployed in the genesis block.
-stakingAddress is addresses of staking contracts of CCO. reward, poc and kir address are the addresses which get a block reward when a block has been created.
+stakingAddress are addresses of staking contracts of CCO. reward, PoC and KIR addresses are the addresses which get a block reward when a block has been created.
 StakingInfo is made every 86400 blocks (stakingInterval) and used in a next interval.
 
 	type StakingInfo struct {
 		BlockNum              uint64
 		CouncilNodeAddrs      []common.Address // Address of Council
 		CouncilStakingAddrs   []common.Address // Address of Staking contract which holds staking balance
-		CouncilRewardAddrs    []common.Address // Address of Council account which will get block reward
+		CouncilRewardAddrs    []common.Address // Address of Council account which will get a block reward
 		KIRAddr               common.Address   // Address of KIR contract
 		PoCAddr               common.Address   // Address of PoC contract
 		UseGini               bool             // configure whether Gini is used or not
 		Gini                  float64          // Gini coefficient
-		CouncilStakingAmounts []uint64         // Staking amounts of Council. They are derived from Staking addresses of council
+		CouncilStakingAmounts []uint64         // StakingAmounts of Council. They are derived from Staking addresses of council
 	}
 
-StakingInfo is managed by stakingManager which has a cache for saving StakingInfo.
-StakingManager calculates block number and interval to find a stakingInfo for current block
+StakingInfo is managed by a stakingManager which has a cache for saving StakingInfos.
+The StakingManager calculates block number with interval to find a stakingInfo for current block
 and returns correct stakingInfo to use.
 
 
@@ -75,7 +73,7 @@ A proposer which has made a current block will get the reward of the block.
 A block reward is calculated by following steps.
 First, calculate totalReward by adding mintingAmount and totalTxFee (unitPrice * gasUsed).
 Second, divide totalReward by ratio (default 34/54/12).
-Last, distribute reward to each address(proposer, PoC, KIR).
+Last, distribute reward to each address (proposer, PoC, KIR).
 
  related struct
  - RewardManager
