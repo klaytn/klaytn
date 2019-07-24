44539af073789432eb5ad306dd8ac5e3853da8b5
Jasmine <32922423+jimni1222@users.noreply.github.com>
2019-07-24 10:52:45 +0900

    Update blockchain/types/accountkey/doc.go
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>

--- blockchain/types/accountkey/doc.go
+++ blockchain/types/accountkey/doc.go
@@ -34,7 +34,7 @@ Each AccountKey type implements the AccountKey interface.
 Source Files
 
 AccountKey related functions and variables are defined in the files listed below.
-  - account_key.go                    : Defines the AccountKey type and the AccountKey interface, and defines the functions related to AccountKey.
+  - account_key.go                    : Defines the AccountKey type, the AccountKey interface and the functions related to AccountKey.
   - account_key_fail.go               : AccountKeyFail is defined according to the AccountKey interface.
   - account_key_legacy.go             : AccountKeyLegacy is defined according to the AccountKey interface.
   - account_key_nil.go                : AccountKeyNil is defined according to the AccountKey interface.
