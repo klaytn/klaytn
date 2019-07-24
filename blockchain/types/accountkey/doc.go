08f54c3d3f1242c1d50b0c50c6eb5ab446fb4d83
Jasmine <32922423+jimni1222@users.noreply.github.com>
2019-07-24 11:07:29 +0900

    Update blockchain/types/accountkey/doc.go
    Co-Authored-By: Ethan (Kyungup Kim) <ethan.kim@groundx.xyz>

--- blockchain/types/accountkey/doc.go
+++ blockchain/types/accountkey/doc.go
@@ -35,7 +35,7 @@ Source Files
 
 AccountKey related functions and variables are defined in the files listed below.
   - account_key.go                    : Defines the AccountKey type, the AccountKey interface and the functions related to AccountKey.
-  - account_key_fail.go               : An AccountKey for fail key is defined. If an account has the fail key, the transaction validation process always fails.
+  - account_key_fail.go               : An AccountKey for fail key is defined. If an account has the fail key, the account's transaction validation process always fails.
   - account_key_legacy.go             : An AccountKey for legacy key is defined.
   - account_key_nil.go                : An AccountKey for nil key is defined.
   - account_key_public.go             : An AccountKey for public key is defined.
