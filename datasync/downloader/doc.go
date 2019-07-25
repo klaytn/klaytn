99a0962f95ae61a73da61e3d74cb1802cacb4e89
Rick <rick.no@groundx.xyz>
2019-07-25 14:29:52 +0900

    Apply suggestions from code review
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    Co-Authored-By: Ethan (Kyungup Kim) <ethan.kim@groundx.xyz>

--- datasync/downloader/doc.go
+++ datasync/downloader/doc.go
@@ -24,7 +24,7 @@ Package downloader contains the manual full chain synchronisation.
 How downloader works
 
 The downloader is responsible for synchronizing up-to-date status from the peers connected to it. To do this,
-download "headers", "bodies", and "receipts" in parallel, merge them through the pipeline, and reflect them in the state trie.
+it downloads "headers", "bodies", and "receipts" in parallel, merges them through the pipeline, and reflect them in the state trie.
 
 Source Files
 
@@ -32,11 +32,11 @@ Downloader related functions and variables are defined in the files listed below
   - api.go              : Console api to get synchronization information.
   - downloader.go       : Functions and variables to sync peer and block. And modules for QOS.
   - downloader_test.go  : Functions for testing the downloader package.
-  - events.go           : Define event type.
-  - metrics.go          : Metric variables for packet transmissions and receptions..
-  - modes.go            : Defines the type for SyncMode. SyncMode includes "FullSync", "FastSync", and "LightSync".
-  - peer.go             : Functions that request a packet to the peer and check and set the network status of the peer.
+  - events.go           : Definitions of event types.
+  - metrics.go          : Metric variables for packet transmissions and receptions.
+  - modes.go            : A definition of type for SyncMode including "FullSync", "FastSync", and "LightSync".
+  - peer.go             : Functions that request a packet to a peer,  check, and set the network status of a peer.
   - queue.go            : Functions for managing and scheduling received headers, bodies, and receipts.
-  - types.go            : Defines the type of downloaded packet
+  - types.go            : Definitions of the types for downloaded packets.
 */
 package downloader
