0d5dbb806d1adc096e1423b60a72d204258ecab0
Jasmine <32922423+jimni1222@users.noreply.github.com>
2019-07-26 11:09:25 +0900

    Update blockchain/types/accountkey/doc.go
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>

--- blockchain/types/accountkey/doc.go
+++ blockchain/types/accountkey/doc.go
@@ -16,7 +16,7 @@
 
 /*
 Package accountkey implements the AccountKey used in Klaytn.
-Inside the package are the types, functions and interfaces associated with the AccountKey.
+Inside the package, the types, functions and interfaces associated with the AccountKey are defined.
 
 Type of AccountKey
 
