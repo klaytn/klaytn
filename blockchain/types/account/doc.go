b1f37ef325156a7f295569d49b9e41f5efc941c5
Jasmine <32922423+jimni1222@users.noreply.github.com>
2019-07-29 11:34:19 +0900

    Update doc.go of package account (#56)
    * Update doc.go of package account
    
    * Remove line
    
    * Reflect review
    
    * Add period
    
    * Edit mistypo
    
    * Update blockchain/types/account/doc.go
    
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    
    * Update blockchain/types/account/doc.go
    
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    
    * fmt

--- blockchain/types/account/doc.go
+++ blockchain/types/account/doc.go
@@ -14,5 +14,36 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package account contains types related to Klaytn accounts.
+/*
+Package account implements Account used in Klaytn.
+
+Inside the package, types, structures, functions, and interfaces associated with the Account are defined.
+
+Type of Account
+
+There are three types of Accounts used in Klaytn.
+
+  - LegacyAccountType
+  - ExternallyOwnedAccountType
+  - SmartContractAccountType
+
+AccountCommon implements the structure and functions common to Klaytn Account, which also implements the Account interface.
+
+EOA (ExternallyOwnedAccount) and SCA (SmartContractAccount) are implemented in a structure that includes AccountCommon.
+
+LegacyAccount is implemented separately according to the account interface.
+
+Source Files
+
+Account related functions and variables are defined in the files listed below.
+  - account.go                  : Defines types, interfaces and functions associated with the Account.
+  - account_common.go           : Data structures and functions that are common to EOA (ExternallyOwnedAccount) and SCA (SmartContractAccount) are defined as AccountCommon.
+  - account_serializer.go       : AccountSerializer is defined for serializing Account.
+  - externally_owned_account.go : ExternallyOwnedAccount containing an AccountCommon is defined.
+  - legacy_account.go           : LegacyAccount that implements the Account interface is defined.
+  - smart_contract_account.go   : SmartContractAccount containing an AccountCommon is defined.
+
+For more information on Account, please refer to the document below.
+https://docs.klaytn.com/klaytn/design/account#klaytn-accounts
+*/
 package account
