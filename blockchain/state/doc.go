bef0101fac4f3d615ef29d0cd19d77cba3a95204
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2019-07-30 09:07:19 +0900

    Reflected review comments

--- blockchain/state/doc.go
+++ blockchain/state/doc.go
@@ -19,25 +19,32 @@
 // Modified and improved for the klaytn development.
 
 /*
-Package state provides a caching layer atop the Klaytn state trie.
+Package state provides a uppermost caching layer of the Klaytn state trie.
 This package is used to read/write stateObject from/to StateDB and it also acts as a bridge between
 the objects and the persistent layer.
 
 Overview of state package
 
-stateObject represents a Klaytn account which is being modified. It is identified by its address.
+stateObject represents a Klaytn account identified by its address.
 Once it is loaded from the persistent layer, it is cached and managed by StateDB.
 StateDB manages stateObjects and operations to them. Most of the operations are done by StateDB,
 instead of direct access to stateObject.
 
 Source Files
 
-  - database.go              : Defines Database and other interfaces used in the package
-  - dump.go                  : Functions to dump the contents of StateDB both in raw format and indented format
-  - journal.go               : journal and state changes to track the list of state modifications since the last state commit
-  - state_object.go          : Implementation of stateObject
-  - state_object_encoder.go  : stateObjectEncoder is used to encode stateObject in parallel manner
-  - statedb.go               : Implementation of StateDB
-  - sync.go                  : Functions to schedule a state trie download
+- database.go              : Defines Database and other interfaces used in the package
+
+- dump.go                  : Functions to dump the contents of StateDB both in raw format and indented format
+
+- journal.go               : journal and state changes to track the list of state modifications since the last state commit
+
+- state_object.go          : Implementation of stateObject
+
+- state_object_encoder.go  : stateObjectEncoder is used to encode stateObject in parallel manner
+
+- statedb.go               : Implementation of StateDB
+
+- sync.go                  : Functions to schedule a state trie download
+
 */
 package state
