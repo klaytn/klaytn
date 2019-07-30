a5931eb91734304f995b8bb3acaee7e392c96a2d
Ethan Kim(김경업) <ethan.kim@groundx.xyz>
2019-07-30 11:14:35 +0900

    review

--- node/sc/bridge_manager_test.go
+++ node/sc/bridge_manager_test.go
@@ -192,7 +192,7 @@ func TestBridgeManager(t *testing.T) {
 					"token", ev.TokenAddress.String(),
 					"requestNonce", ev.RequestNonce)
 
-				done, err := bridge.IsHandledRequestTx(nil, ev.Raw.TxHash)
+				done, err := bridge.HandledRequestTx(nil, ev.Raw.TxHash)
 				assert.NoError(t, err)
 				assert.Equal(t, false, done)
 
@@ -233,7 +233,7 @@ func TestBridgeManager(t *testing.T) {
 				}
 
 				wg.Done()
-				done, err = bridge.IsHandledRequestTx(nil, ev.Raw.TxHash)
+				done, err = bridge.HandledRequestTx(nil, ev.Raw.TxHash)
 				assert.NoError(t, err)
 				assert.Equal(t, true, done)
 
