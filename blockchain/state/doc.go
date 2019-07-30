a0488885df33dd7f90d8046d2d0481650bb41719
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2019-07-30 16:16:54 +0900

    state: updated blockchain/state/doc.go (#81)
    * Updated blockchain/state/doc.go
    
    * Fixed gofmt failure
    
    * Reflected review comments
    
    * Reflected review comments
    
    * Reflected review comments
    
    * Fixed for godoc
    
    * Update blockchain/state/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>

--- blockchain/state/doc.go
+++ blockchain/state/doc.go
@@ -18,5 +18,28 @@
 // This file is derived from core/state/statdb.go (2018/06/04).
 // Modified and improved for the klaytn development.
 
-// Package state provides a caching layer atop the Klaytn state trie.
+/*
+Package state provides an uppermost caching layer of the Klaytn state trie.
+This package is used to read/write stateObject from/to StateDB and it also acts as a bridge between
+the objects and the persistent layer.
+
+Overview of state package
+
+stateObject represents a Klaytn account identified by its address.
+Once it is loaded from the persistent layer, it is cached and managed by StateDB.
+
+StateDB caches stateObjects and mediates the operations to them.
+
+
+Source Files
+
+Related functions and variables are defined in the files listed below
+  - database.go              : Defines Database and other interfaces used in the package
+  - dump.go                  : Functions to dump the contents of StateDB both in raw format and indented format
+  - journal.go               : journal and state changes to track the list of state modifications since the last state commit
+  - state_object.go          : Implementation of stateObject
+  - state_object_encoder.go  : stateObjectEncoder is used to encode stateObject in parallel manner
+  - statedb.go               : Implementation of StateDB
+  - sync.go                  : Functions to schedule a state trie download
+*/
 package state
