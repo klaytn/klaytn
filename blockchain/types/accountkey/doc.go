98398829c5b7ddb7b9bcb2db42e44345b5b53916
Jasmine <32922423+jimni1222@users.noreply.github.com>
2019-07-30 13:19:13 +0900

    Update blockchain/types/accountkey/doc.go (#55)
    * Update blockchain/types/accountkey/doc.go
    
    * Modify form
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Add period
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    
    * Reflect review
    
    * Add fail key description
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Ethan (Kyungup Kim) <ethan.kim@groundx.xyz>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Jeongho Albert Nah <jeongho.nah@gmail.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    * Update blockchain/types/accountkey/doc.go
    
    * Update blockchain/types/accountkey/doc.go
    
    * Update blockchain/types/accountkey/doc.go
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    
    * Update blockchain/types/accountkey/doc.go
    
    * fmt

--- blockchain/types/accountkey/doc.go
+++ blockchain/types/accountkey/doc.go
@@ -14,5 +14,36 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package accountkey contains types related to keys in Klaytn accounts.
+/*
+Package accountkey implements the AccountKey used in Klaytn.
+Inside the package, types, functions and interfaces associated with the AccountKey are defined.
+
+Type of AccountKey
+
+The AccountKey types used in Klaytn are as follows:
+  - AccountKeyTypeNil
+  - AccountKeyTypeLegacy
+  - AccountKeyTypePublic
+  - AccountKeyTypeFail
+  - AccountKeyTypeWeightedMultiSig
+  - AccountKeyTypeRoleBased
+
+Each AccountKey type implements the AccountKey interface.
+
+Source Files
+
+AccountKey related functions and variables are defined in the files listed below.
+  - account_key.go                    : Defines the AccountKey types, the AccountKey interface and the functions related to AccountKey.
+  - account_key_fail.go               : An AccountKey for AccountKeyFail type is defined. If an account has the fail key, the account's transaction validation process always fails.
+  - account_key_legacy.go             : An AccountKey for AccountKeyLegacy type is defined. If an account has the legacy key, the account's key pair should be coupled with its address.
+  - account_key_nil.go                : An AccountKey for AccountKeyNil type is defined. The nil key is used only for TxTypeAccountUpdate transactions representing an empty key.
+  - account_key_public.go             : An AccountKey for AccountKeyPublic type is defined. If an account contains a public key as an account key, the public key will be used in the account's transaction validation process.
+  - account_key_role_based.go         : An AccountKey for AccountKeyRoleBased type is defined. AccountKeyRoleBased contains keys that have three roles: RoleTransaction, RoleAccountUpdate, and RoleFeePayer. If an account has a role-based key that consists of more than one key, the account's transaction validation process will use one key in the role-based key depends on the transaction type.
+  - account_key_serializer.go         : AccountKeySerializer is defined for serialization of AccountKey.
+  - account_key_weighted_multi_sig.go : An AccountKey for AccountKeyWeightedMultiSig type is defined. AccountKeyWeightedMultiSig contains Threshold and WeightedPublicKeys.
+  - public_key.go                     : PublicKeySerializable is defined for serialization of public key.
+
+For more information on AccountKey, please see the document below.
+https://docs.klaytn.com/klaytn/design/account#account-key
+*/
 package accountkey
