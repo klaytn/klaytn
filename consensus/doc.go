4250f9e116eb7388de45610c32411ec5c6e6a84d
Andy Byungchul Lee <37580675+andybclee@users.noreply.github.com>
2019-08-02 10:26:53 +0900

    Update consensus/doc.go
    Thanks!
    
    Co-Authored-By: Melvin Junhee Woo <melvin.woo@groundx.xyz>

--- consensus/doc.go
+++ consensus/doc.go
@@ -21,7 +21,7 @@
 /*
 Package consensus defines interfaces for consensus engines and ChainReader.
 
-Klaytn currently uses istanbul BFT engine on the mainnet, but PoA based clique engine is also can be used.
+Klaytn currently uses istanbul BFT engine on the mainnet, but PoA based clique engine also can be used.
 Traditional PoW engine(gxhash) is used by legacy test codes but it is deprecated and is not recommended to use.
 
 By implementing the Engine interface, new consensus engine can be added and used in Klaytn.
