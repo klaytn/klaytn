b5a24efd2bade9d6e83cfd04616407d58f78ca3c
Andy Byungchul Lee <andy.lee@groundx.xyz>
2019-08-02 14:22:00 +0900

    Add more description of how block generation continues

--- consensus/clique/doc.go
+++ consensus/clique/doc.go
@@ -24,8 +24,8 @@ In clique, only pre-selected nodes can propose a block. The list of these nodes 
 The sequence of proposers is decided by the block number and the number of nodes which can propose a block. If a proposer proposes a block in its turn (in-turn), it will have a blockscore(formerly known as difficulty) of 2.
 If a proposer proposer propose a block in other node's turn (out-of-turn), it will have a blockscore of 1.
 
-If an in-turn proposer didn't propose a block for some time, other nodes start to propose their block with a blockscore of 1.
-
-Based on this algorithm, the rule of the `longest chain wins` and the network's liveness can be achieved.
+If an in-turn proposer didn't propose a block for some time, other nodes start to propose their block with a blockscore of 1. So the block generation can continue.
+But there can be a fork because several nodes can propose a block at the same time.
+In this case, a block reorganization will happen based on total blockscore (the sum of all blockscore from the genesis block to the current one).
 */
 package clique
