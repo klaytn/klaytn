80d10df5037a07b355dab5794e2b765a90181bdb
Andy Byungchul Lee <37580675+andybclee@users.noreply.github.com>
2019-08-05 15:39:39 +0900

    Update consensus/clique/doc.go
    Co-Authored-By: Melvin Junhee Woo <melvin.woo@groundx.xyz>

--- consensus/clique/doc.go
+++ consensus/clique/doc.go
@@ -22,7 +22,7 @@ Consensus Engine Overview
 In clique, only pre-selected nodes can propose a block. The list of these nodes are described in the header's extra field and the list can be changed by calling Propose() API.
 
 The sequence of proposers is decided by the block number and the number of nodes which can propose a block. If a proposer proposes a block in its turn (in-turn), it will have a blockscore(formerly known as difficulty) of 2.
-If a proposer proposer propose a block in other node's turn (out-of-turn), it will have a blockscore of 1.
+If a proposer proposes a block in other node's turn (out-of-turn), it will have a blockscore of 1.
 
 If an in-turn proposer didn't propose a block for some time, other nodes start to propose their block with a blockscore of 1. So the block generation can continue.
 But there can be a fork because several nodes can propose a block at the same time.
