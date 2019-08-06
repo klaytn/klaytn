c4b5ab62a21dc1d9a02c72aba54129591c467162
Andy Byungchul Lee <37580675+andybclee@users.noreply.github.com>
2019-08-06 10:22:03 +0900

    Update console/jsre/doc.go
    Co-Authored-By: Melvin Junhee Woo <melvin.woo@groundx.xyz>

--- console/jsre/doc.go
+++ console/jsre/doc.go
@@ -36,6 +36,6 @@ Source Files
 Each file provides following features
  - completion.go	: Provides functions for keyword completion
  - jsre.go	: Wraps otto Javascript interpreter and provides an event loop
- - pretty.go	: Print result to the standard output in more readable way
+ - pretty.go	: Prints result to the standard output in more readable way
 */
 package jsre
