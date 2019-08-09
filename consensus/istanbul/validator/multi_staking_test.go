994757eba6a5d7c3c6528419fc5979f00545e501
laggu <sebastian.ra@groundx.xyz>
2019-08-09 14:02:40 +0900

    add Testcode for multi staking contract

---
+++ consensus/istanbul/validator/multi_staking_test.go
@@ -0,0 +1,83 @@
+package validator
+
+import (
+	"github.com/klaytn/klaytn/common"
+	"github.com/klaytn/klaytn/consensus/istanbul"
+	"github.com/klaytn/klaytn/reward"
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"testing"
+)
+
+func newTestWeightedCouncil(nodeAddrs []common.Address) *weightedCouncil {
+	return NewWeightedCouncil(nodeAddrs, nil, make([]uint64, len(nodeAddrs)), nil, istanbul.WeightedRandom, 0, 0, 0, nil)
+}
+
+func TestWeightedCouncil_getStakingAmountsOfValidators(t *testing.T) {
+	testCases := []struct {
+		validators             []common.Address
+		stakingInfo            *reward.StakingInfo
+		expectedStakingAmounts []float64
+	}{
+		{
+			[]common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103))},
+			&reward.StakingInfo{
+				CouncilNodeAddrs:      []common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103))},
+				CouncilRewardAddrs:    []common.Address{common.BigToAddress(big.NewInt(301)), common.BigToAddress(big.NewInt(302)), common.BigToAddress(big.NewInt(303))},
+				CouncilStakingAmounts: []uint64{5000000, 5000000, 5000000},
+			},
+			[]float64{5000000, 5000000, 5000000},
+		},
+		{
+			[]common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103))},
+			&reward.StakingInfo{
+				CouncilNodeAddrs:      []common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103))},
+				CouncilRewardAddrs:    []common.Address{common.BigToAddress(big.NewInt(301)), common.BigToAddress(big.NewInt(302)), common.BigToAddress(big.NewInt(303))},
+				CouncilStakingAmounts: []uint64{7000000, 5000000, 10000000},
+			},
+			[]float64{7000000, 5000000, 10000000},
+		},
+		{
+			[]common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103)), common.BigToAddress(big.NewInt(104))},
+			&reward.StakingInfo{
+				CouncilNodeAddrs:      []common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103)), common.BigToAddress(big.NewInt(104)), common.BigToAddress(big.NewInt(901))},
+				CouncilRewardAddrs:    []common.Address{common.BigToAddress(big.NewInt(301)), common.BigToAddress(big.NewInt(302)), common.BigToAddress(big.NewInt(303)), common.BigToAddress(big.NewInt(304)), common.BigToAddress(big.NewInt(301))},
+				CouncilStakingAmounts: []uint64{5000000, 5000000, 5000000, 5000000, 5000000},
+			},
+			[]float64{10000000, 5000000, 5000000, 5000000},
+		},
+		{
+			[]common.Address{common.BigToAddress(big.NewInt(104)), common.BigToAddress(big.NewInt(103)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(101))},
+			&reward.StakingInfo{
+				CouncilNodeAddrs:      []common.Address{common.BigToAddress(big.NewInt(101)), common.BigToAddress(big.NewInt(102)), common.BigToAddress(big.NewInt(103)), common.BigToAddress(big.NewInt(104)), common.BigToAddress(big.NewInt(901)), common.BigToAddress(big.NewInt(902))},
+				CouncilRewardAddrs:    []common.Address{common.BigToAddress(big.NewInt(301)), common.BigToAddress(big.NewInt(302)), common.BigToAddress(big.NewInt(303)), common.BigToAddress(big.NewInt(304)), common.BigToAddress(big.NewInt(301)), common.BigToAddress(big.NewInt(302))},
+				CouncilStakingAmounts: []uint64{5000000, 5000000, 5000000, 5000000, 5000000, 5000000},
+			},
+			[]float64{10000000, 10000000, 5000000, 5000000},
+		},
+	}
+	for _, testCase := range testCases {
+		council := newTestWeightedCouncil(testCase.validators)
+
+		weightedValidators, stakingAmounts, err := council.getStakingAmountsOfValidators(testCase.stakingInfo)
+
+		assert.NoError(t, err)
+		assert.Equal(t, len(testCase.validators), len(weightedValidators))
+		for i := 0; i < len(testCase.validators); i++ {
+			assert.Contains(t, testCase.validators, weightedValidators[i].address)
+		}
+		assert.Equal(t, testCase.expectedStakingAmounts, stakingAmounts)
+	}
+}
+
+func TestWeightedCouncil_calcTotalAmount(t *testing.T) {
+
+}
+
+func TestWeightedCouncil_calcWeight(t *testing.T) {
+
+}
+
+func TestWeightedCouncil_validatorWeightWithStakingInfo(t *testing.T) {
+
+}
