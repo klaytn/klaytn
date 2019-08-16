cdcabe4b4f0f69f86bd705a837475f5329bb5fc9
Ethan Kim(김경업) <ethan.kim@groundx.xyz>
2019-08-16 14:30:06 +0900

    Update solc version to 0.5.11

--- contracts/externals/openzeppelin-solidity/contracts/ownership/Ownable.sol
+++ contracts/externals/openzeppelin-solidity/contracts/ownership/Ownable.sol
@@ -1,75 +1,75 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 /**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be aplied to your functions to restrict their use to
+ * the owner.
  */
 contract Ownable {
-  address private _owner;
+    address private _owner;
 
-  event OwnershipTransferred(
-    address indexed previousOwner,
-    address indexed newOwner
-  );
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() internal {
-    _owner = msg.sender;
-    emit OwnershipTransferred(address(0), _owner);
-  }
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
 
-  /**
-   * @return the address of the owner.
-   */
-  function owner() public view returns(address) {
-    return _owner;
-  }
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(isOwner());
-    _;
-  }
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
 
-  /**
-   * @return true if `msg.sender` is the owner of the contract.
-   */
-  function isOwner() public view returns(bool) {
-    return msg.sender == _owner;
-  }
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
 
-  /**
-   * @dev Allows the current owner to relinquish control of the contract.
-   * @notice Renouncing to ownership will leave the contract without an owner.
-   * It will not be possible to call the functions with the `onlyOwner`
-   * modifier anymore.
-   */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
-  }
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * > Note: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    _transferOwnership(newOwner);
-  }
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
 
-  /**
-   * @dev Transfers control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function _transferOwnership(address newOwner) internal {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(_owner, newOwner);
-    _owner = newOwner;
-  }
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
 }
--- contracts/externals/openzeppelin-solidity/contracts/payment/PaymentSplitter.sol
+++ contracts/externals/openzeppelin-solidity/contracts/payment/PaymentSplitter.sol
@@ -1,116 +1,131 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 import "../math/SafeMath.sol";
 
 /**
  * @title PaymentSplitter
- * @dev This contract can be used when payments need to be received by a group
- * of people and split proportionately to some number of shares they own.
+ * @dev This contract allows to split Ether payments among a group of accounts. The sender does not need to be aware
+ * that the Ether will be split in this way, since it is handled transparently by the contract.
+ *
+ * The split can be in equal parts or in any other arbitrary proportion. The way this is specified is by assigning each
+ * account to a number of shares. Of all the Ether that this contract receives, each account will then be able to claim
+ * an amount proportional to the percentage of total shares they were assigned.
+ *
+ * `PaymentSplitter` follows a _pull payment_ model. This means that payments are not automatically forwarded to the
+ * accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the `release`
+ * function.
  */
 contract PaymentSplitter {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  event PayeeAdded(address account, uint256 shares);
-  event PaymentReleased(address to, uint256 amount);
-  event PaymentReceived(address from, uint256 amount);
+    event PayeeAdded(address account, uint256 shares);
+    event PaymentReleased(address to, uint256 amount);
+    event PaymentReceived(address from, uint256 amount);
 
-  uint256 private _totalShares;
-  uint256 private _totalReleased;
+    uint256 private _totalShares;
+    uint256 private _totalReleased;
 
-  mapping(address => uint256) private _shares;
-  mapping(address => uint256) private _released;
-  address[] private _payees;
+    mapping(address => uint256) private _shares;
+    mapping(address => uint256) private _released;
+    address[] private _payees;
 
-  /**
-   * @dev Constructor
-   */
-  constructor(address[] payees, uint256[] shares) public payable {
-    require(payees.length == shares.length);
-    require(payees.length > 0);
+    /**
+     * @dev Creates an instance of `PaymentSplitter` where each account in `payees` is assigned the number of shares at
+     * the matching position in the `shares` array.
+     *
+     * All addresses in `payees` must be non-zero. Both arrays must have the same non-zero length, and there must be no
+     * duplicates in `payees`.
+     */
+    constructor (address[] memory payees, uint256[] memory shares) public payable {
+        // solhint-disable-next-line max-line-length
+        require(payees.length == shares.length, "PaymentSplitter: payees and shares length mismatch");
+        require(payees.length > 0, "PaymentSplitter: no payees");
 
-    for (uint256 i = 0; i < payees.length; i++) {
-      _addPayee(payees[i], shares[i]);
+        for (uint256 i = 0; i < payees.length; i++) {
+            _addPayee(payees[i], shares[i]);
+        }
     }
-  }
 
-  /**
-   * @dev payable fallback
-   */
-  function () external payable {
-    emit PaymentReceived(msg.sender, msg.value);
-  }
+    /**
+     * @dev The Ether received will be logged with `PaymentReceived` events. Note that these events are not fully
+     * reliable: it's possible for a contract to receive Ether without triggering this function. This only affects the
+     * reliability of the events, and not the actual splitting of Ether.
+     *
+     * To learn more about this see the Solidity documentation for [fallback functions].
+     *
+     * [fallback functions]: https://solidity.readthedocs.io/en/latest/contracts.html#fallback-function
+     */
+    function () external payable {
+        emit PaymentReceived(msg.sender, msg.value);
+    }
 
-  /**
-   * @return the total shares of the contract.
-   */
-  function totalShares() public view returns(uint256) {
-    return _totalShares;
-  }
+    /**
+     * @dev Getter for the total shares held by payees.
+     */
+    function totalShares() public view returns (uint256) {
+        return _totalShares;
+    }
 
-  /**
-   * @return the total amount already released.
-   */
-  function totalReleased() public view returns(uint256) {
-    return _totalReleased;
-  }
+    /**
+     * @dev Getter for the total amount of Ether already released.
+     */
+    function totalReleased() public view returns (uint256) {
+        return _totalReleased;
+    }
 
-  /**
-   * @return the shares of an account.
-   */
-  function shares(address account) public view returns(uint256) {
-    return _shares[account];
-  }
+    /**
+     * @dev Getter for the amount of shares held by an account.
+     */
+    function shares(address account) public view returns (uint256) {
+        return _shares[account];
+    }
 
-  /**
-   * @return the amount already released to an account.
-   */
-  function released(address account) public view returns(uint256) {
-    return _released[account];
-  }
+    /**
+     * @dev Getter for the amount of Ether already released to a payee.
+     */
+    function released(address account) public view returns (uint256) {
+        return _released[account];
+    }
 
-  /**
-   * @return the address of a payee.
-   */
-  function payee(uint256 index) public view returns(address) {
-    return _payees[index];
-  }
+    /**
+     * @dev Getter for the address of the payee number `index`.
+     */
+    function payee(uint256 index) public view returns (address) {
+        return _payees[index];
+    }
 
-  /**
-   * @dev Release one of the payee's proportional payment.
-   * @param account Whose payments will be released.
-   */
-  function release(address account) public {
-    require(_shares[account] > 0);
+    /**
+     * @dev Triggers a transfer to `account` of the amount of Ether they are owed, according to their percentage of the
+     * total shares and their previous withdrawals.
+     */
+    function release(address payable account) public {
+        require(_shares[account] > 0, "PaymentSplitter: account has no shares");
 
-    uint256 totalReceived = address(this).balance.add(_totalReleased);
-    uint256 payment = totalReceived.mul(
-      _shares[account]).div(
-        _totalShares).sub(
-          _released[account]
-    );
+        uint256 totalReceived = address(this).balance.add(_totalReleased);
+        uint256 payment = totalReceived.mul(_shares[account]).div(_totalShares).sub(_released[account]);
 
-    require(payment != 0);
+        require(payment != 0, "PaymentSplitter: account is not due payment");
 
-    _released[account] = _released[account].add(payment);
-    _totalReleased = _totalReleased.add(payment);
+        _released[account] = _released[account].add(payment);
+        _totalReleased = _totalReleased.add(payment);
 
-    account.transfer(payment);
-    emit PaymentReleased(account, payment);
-  }
+        account.transfer(payment);
+        emit PaymentReleased(account, payment);
+    }
 
-  /**
-   * @dev Add a new payee to the contract.
-   * @param account The address of the payee to add.
-   * @param shares_ The number of shares owned by the payee.
-   */
-  function _addPayee(address account, uint256 shares_) private {
-    require(account != address(0));
-    require(shares_ > 0);
-    require(_shares[account] == 0);
+    /**
+     * @dev Add a new payee to the contract.
+     * @param account The address of the payee to add.
+     * @param shares_ The number of shares owned by the payee.
+     */
+    function _addPayee(address account, uint256 shares_) private {
+        require(account != address(0), "PaymentSplitter: account is the zero address");
+        require(shares_ > 0, "PaymentSplitter: shares are 0");
+        require(_shares[account] == 0, "PaymentSplitter: account already has shares");
 
-    _payees.push(account);
-    _shares[account] = shares_;
-    _totalShares = _totalShares.add(shares_);
-    emit PayeeAdded(account, shares_);
-  }
+        _payees.push(account);
+        _shares[account] = shares_;
+        _totalShares = _totalShares.add(shares_);
+        emit PayeeAdded(account, shares_);
+    }
 }
