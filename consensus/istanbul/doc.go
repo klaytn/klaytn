d2ee494b30ba33375e54e042f5edfa5b28b8156b
Andy Byungchul Lee <37580675+andybclee@users.noreply.github.com>
2019-09-03 13:25:44 +0900

    Add more description for istanbul package (#182)
    * Add description for the package istanbul on its doc.go
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
    Co-Authored-By: Melvin Junhee Woo <melvin.woo@groundx.xyz>

--- consensus/istanbul/doc.go
+++ consensus/istanbul/doc.go
@@ -14,5 +14,24 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package istanbul implements the istanbul consensus engine.
+/*
+Package istanbul is a BFT based consensus engine which implements consensus/Engine interface.
+Istanbul engine was inspired by Clique POA, Hyperledger's SBFT, Tendermint, HydraChain and NCCU BFT.
+
+Istanbul engine is using 3-phase consensus and it can tolerate F faulty nodes where N = 3F + 1
+
+In Klaytn, it is being used as the main consensus engine after modification for supports of Committee, Reward and Governance.
+Package istanbul has three sub-packages, core, backend, and validator. Please refer to each package's doc.go for more information.
+
+Source Files
+
+Various interfaces, constants and utility functions for Istanbul consensus engine
+ - `backend.go`: Defines Backend interface which provides application specific functions for Istanbul core
+ - `config.go`: Provides default configuration for Istanbul engine
+ - `errors.go`: Defines three errors used in Istanbul engine
+ - `events.go`: Defines events which are used for Istanbul engine communication
+ - `types.go`: Defines message structs such as Proposal, Request, View, Preprepare, Subject and ConsensusMsg
+ - `utils.go`: Provides three utility functions: RLPHash, GetSignatureAddress and CheckValidatorSignature
+ - `validator.go`: Defines Validator, ValidatorSet interfaces and Validators, ProposalSelector types
+*/
 package istanbul
