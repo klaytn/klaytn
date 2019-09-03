e050c673782f28123d4ee821511d0ae039e2fb5a
Ethan Kim(김경업) <ethan.kim@groundx.xyz>
2019-09-03 16:25:09 +0900

    clean code

--- contracts/bridge/BridgeOperator.sol
+++ contracts/bridge/BridgeOperator.sol
@@ -31,14 +31,13 @@ contract BridgeOperator is Ownable {
         mapping(bytes32 => uint8) voteCounts; // <sha3(type, args, nonce), uint8>
     }
 
-    mapping(address => bool) public operators;
-    address[] public operatorList;
-
     mapping(uint8 => mapping (uint64 => VotesData)) private votes; // <voteType, <nonce, VotesData>
     mapping(uint64 => bool) public closedValueTransferVotes; // <nonce, bool>
 
-    mapping(uint8 => uint8) public operatorThresholds; // <vote type, uint8>
+    mapping(address => bool) public operators;
+    address[] public operatorList;
 
+    mapping(uint8 => uint8) public operatorThresholds; // <vote type, uint8>
     uint64 public configurationNonce;
 
     enum VoteType {
