c46081784e35dfc3f367960a14b2fe83a75d75ed
Andy Byungchul Lee <37580675+andybclee@users.noreply.github.com>
2019-09-03 13:25:10 +0900

    Add description for `backend` package (#181)
    * Add description for `backend` package
    
    Co-Authored-By: Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>

--- consensus/istanbul/backend/doc.go
+++ consensus/istanbul/backend/doc.go
@@ -14,5 +14,19 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package backend implements backend for istanbul consensus engine.
+/*
+Package backend defines backend struct which implements Backend interface of Istanbul consensus engine.
+backend struct works as a backbone of the consensus engine having Istanbul core,
+required objects to get information for making a consensus, recent messages and a broadcaster to send its message to peer nodes.
+
+Source Files
+
+Implementation of Backend interface and APIs are included in this package
+ - `api.go`: Implements APIs which provide the states of Istanbul
+ - `backend.go`: Defines backend struct which implements Backend interface working as a backbone of the consensus engine
+ - `engine.go`: Implements various backend methods especially for verifying and building header information
+ - `handler.go`: Implements backend methods for handling messages and broadcaster
+ - `snapshot.go`: Defines snapshot struct which handles votes from nodes and makes governance changes
+
+*/
 package backend
