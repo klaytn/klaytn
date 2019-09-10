112bce256d93558c1f151d0ab890d821c202e3c1
Aidan <aidan.kwon@groundx.xyz>
2019-09-10 16:26:56 +0900

    add copyright

--- node/sc/mainbridge_test.go
+++ node/sc/mainbridge_test.go
@@ -1,3 +1,19 @@
+// Copyright 2019 The klaytn Authors
+// This file is part of the klaytn library.
+//
+// The klaytn library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The klaytn library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
+
 package sc
 
 import (
@@ -92,9 +108,11 @@ func TestMainBridge_basic(t *testing.T) {
 	assert.Equal(t, txPool, mBridge.txPool)
 	assert.NotNil(t, mBridge.rpcServer.GetServices()["klay"])
 
-	// Start and stop MainBridge
+	// Start MainBridge and stop later
 	if err := mBridge.Start(p2p.SingleChannelServer{}); err != nil {
 		t.Fatal(err)
 	}
 	defer mBridge.Stop()
+
+	//TODO more test code
 }
