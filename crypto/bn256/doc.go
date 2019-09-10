e39727bba16480ad05f0967ff0fd24ed075fc682
Andy Byungchul Lee <37580675+andybclee@users.noreply.github.com>
2019-09-10 13:29:27 +0900

    Update crypto/bn256/doc.go
    Co-Authored-By: Melvin Junhee Woo <melvin.woo@groundx.xyz>

--- crypto/bn256/doc.go
+++ crypto/bn256/doc.go
@@ -10,7 +10,7 @@ Bilinear groups are the basis of many of the new cryptographic protocols that ha
 
 This package specifically implements the Optimal Ate pairing over a 256-bit Barreto-Naehrig curve as described in http://cryptojedi.org/papers/dclxvi-20100714.pdf. Its output is compatible with the implementation described in that paper.
 
-This package previously claimed to operate at a 128-bit security level. However, recent improvements in attacks mean that is no longer true. See https://moderncrypto.org/mail-archive/curves/2016/000740.html.
+This package previously claimed to operate at a 128-bit security level. However, recent improvements in attacks mean that it is no longer true. See https://moderncrypto.org/mail-archive/curves/2016/000740.html.
 
 Initial package from Google is deprecated due to its weakened security and Klaytn is using the more complete implementation from Cloudflare
 
