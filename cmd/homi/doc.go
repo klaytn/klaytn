ba503ea7c1c3a456ea67f40a74e80d562e0525b5
Andy Byungchul Lee <andy.lee@groundx.xyz>
2019-10-10 16:58:18 +0900

    Add descriptions for homi packages

---
+++ cmd/homi/doc.go
@@ -0,0 +1,31 @@
+// Copyright 2018 The klaytn Authors
+// Copyright 2016 The go-ethereum Authors
+// This file is part of go-ethereum.
+//
+// Copyright 2017 AMIS Technologies
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+//
+
+/*
+homi is a command line interface which helps to create configuration files for a new network
+homi supports various options for different deployment modes, number of nodes, etc.
+
+Source Files
+
+Each file contains the following contents
+ - main.go : Runs the application as an entry point
+*/
+package main
