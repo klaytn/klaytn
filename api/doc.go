b1411c96b780c88b7179c96df0d4838fa4ce8a90
Donghwan Aidan Kwon <48199072+aidan-kwon@users.noreply.github.com>
2019-10-14 16:31:09 +0900

    Update doc.go for api package (#286)

--- api/doc.go
+++ api/doc.go
@@ -18,5 +18,28 @@
 // This file is derived from internal/ethapi/api.go (2018/06/04).
 // Modified and improved for the klaytn development.
 
-// Package api implements the general Klaytn API functions.
+/*
+Package api implements the general Klaytn API functions.
+
+Overview of api package
+
+This package provides various APIs to access the data of the Klaytn node.
+Remote users can interact with Klyatn by calling these APIs instead of IPC.
+APIs are grouped by access modifiers (public or private) and namespaces (klay, txpool, debug and personal).
+
+Source Files
+
+  - addrlock.go                    : implements Addrlocker which prevents another tx getting the same nonce through API.
+  - api_private_account.go         : provides private APIs to access accounts managed by the node.
+  - api_private_debug.go           : provides private APIs exposed over the debugging node.
+  - api_public_account.go          : provides public APIs to access accounts managed by the node.
+  - api_public_blockchain.go       : provides public APIs to access the Klaytn blockchain.
+  - api_public_cypress.go          : provides public APIs to return specific information of Klaytn Cypress network.
+  - api_public_debug.go            : provides public APIs exposed over the debugging node.
+  - api_public_klay.go             : provides public APIs to access Klaytn related data.
+  - api_public_net.go              : provides public APIs to offer network related RPC methods.
+  - api_public_transaction_pool.go : provides public APIs having "klay" namespace to access transaction pool data.
+  - api_public_tx_pool.go          : provides public APIs having "txpool" namespace to access transaction pool data.
+  - backend.go                     : provides the common API services.
+*/
 package api
