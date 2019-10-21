dfe8d446c9877fed33b4025b8d70cb9ce2e91b02
Andy Byungchul Lee <andy.lee@groundx.xyz>
2019-10-21 16:55:21 +0900

    temporary

--- utils/build/doc.go
+++ utils/build/doc.go
@@ -14,5 +14,11 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package build contains the utility functions for build environment.
+/*
+Package build contains the utility functions for build environment.
+
+Source Files
+
+
+*/
 package build
