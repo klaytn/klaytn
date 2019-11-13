51729427679392f81b47e9bdd737bd95f0799c15
Rick <rick.no@groundx.xyz>
2019-11-14 00:10:33 +0900

    add multichannel peer test (#377)

--- networks/p2p/peer_test.go
+++ networks/p2p/peer_test.go
@@ -67,6 +67,37 @@ func testPeer(protos []Protocol) (func(), *conn, *Peer, <-chan error) {
 	return closer, c2, peer, errc
 }
 
+func testPeerWithRWs(protos []Protocol, channelSize int) (func(), []*conn, *Peer, <-chan error) {
+	serverSideConn := make([]*conn, 0, channelSize)
+	peerSideConn := make([]*conn, 0, channelSize)
+
+	for i := 0; i < channelSize; i++ {
+		fd1, fd2 := net.Pipe()
+		c1 := &conn{fd: fd1, transport: newTestTransport(randomID(), fd1, true)}
+		c2 := &conn{fd: fd2, transport: newTestTransport(randomID(), fd2, true)}
+		for _, p := range protos {
+			c1.caps = append(c1.caps, p.cap())
+			c2.caps = append(c2.caps, p.cap())
+		}
+		serverSideConn = append(serverSideConn, c1)
+		peerSideConn = append(peerSideConn, c2)
+	}
+
+	peer, _ := newPeer(serverSideConn, protos, defaultRWTimerConfig)
+	errc := make(chan error, 1)
+	go func() {
+		_, err := peer.runWithRWs()
+		errc <- err
+	}()
+
+	closer := func() {
+		for _, conn := range peerSideConn {
+			conn.close(errors.New("close func called"))
+		}
+	}
+	return closer, peerSideConn, peer, errc
+}
+
 func TestPeerProtoReadMsg(t *testing.T) {
 	proto := Protocol{
 		Name:   "a",
@@ -188,6 +219,159 @@ func TestPeerDisconnectRace(t *testing.T) {
 		// In some cases, simulate remote requesting a disconnect.
 		if maybe() {
 			go SendItems(rw, discMsg, DiscQuitting)
+		}
+
+		select {
+		case <-disc:
+		case <-time.After(2 * time.Second):
+			// Peer.run should return quickly. If it doesn't the Peer
+			// goroutines are probably deadlocked. Call panic in order to
+			// show the stacks.
+			panic("Peer.run took to long to return.")
+		}
+	}
+}
+
+func TestMultiChannelPeerProtoReadMsg(t *testing.T) {
+	proto := Protocol{
+		Name:   "a",
+		Length: 5,
+		RunWithRWs: func(peer *Peer, rws []MsgReadWriter) error {
+			for _, rw := range rws {
+				if err := ExpectMsg(rw, 2, []uint{1}); err != nil {
+					t.Error(err)
+				}
+				if err := ExpectMsg(rw, 3, []uint{2}); err != nil {
+					t.Error(err)
+				}
+				if err := ExpectMsg(rw, 4, []uint{3}); err != nil {
+					t.Error(err)
+				}
+			}
+			return nil
+		},
+	}
+
+	closer, rws, _, errc := testPeerWithRWs([]Protocol{proto}, 2)
+	defer closer()
+
+	for _, rw := range rws {
+		Send(rw, baseProtocolLength+2, []uint{1})
+		Send(rw, baseProtocolLength+3, []uint{2})
+		Send(rw, baseProtocolLength+4, []uint{3})
+	}
+
+	select {
+	case err := <-errc:
+		if err != errProtocolReturned {
+			t.Errorf("peer returned error: %v", err)
+		}
+	case <-time.After(2 * time.Second):
+		t.Errorf("receive timeout")
+	}
+}
+
+func TestMultiChannelPeerProtoEncodeMsg(t *testing.T) {
+	proto := Protocol{
+		Name:   "a",
+		Length: 2,
+		RunWithRWs: func(peer *Peer, rws []MsgReadWriter) error {
+			for _, rw := range rws {
+				if err := SendItems(rw, 2); err == nil {
+					t.Error("expected error for out-of-range msg code, got nil")
+				}
+				if err := SendItems(rw, 1, "foo", "bar"); err != nil {
+					t.Errorf("write error: %v", err)
+				}
+			}
+			return nil
+		},
+	}
+	closer, rws, _, _ := testPeerWithRWs([]Protocol{proto}, 2)
+	defer closer()
+
+	for _, rw := range rws {
+		if err := ExpectMsg(rw, 17, []string{"foo", "bar"}); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestMultiChannelPeerPing(t *testing.T) {
+	closer, rws, _, _ := testPeerWithRWs(nil, 2)
+	defer closer()
+
+	for _, rw := range rws {
+		if err := SendItems(rw, pingMsg); err != nil {
+			t.Fatal(err)
+		}
+		if err := ExpectMsg(rw, pongMsg, nil); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestMultiChannelPeerDisconnect(t *testing.T) {
+	channelSize := 2
+	for i := 0; i < channelSize; i++ {
+		closer, rws, _, disc := testPeerWithRWs(nil, channelSize)
+		defer closer()
+
+		if err := SendItems(rws[i], discMsg, DiscQuitting); err != nil {
+			t.Fatal(err)
+		}
+
+		select {
+		case reason := <-disc:
+			if reason != DiscQuitting {
+				t.Errorf("run returned wrong reason: got %v, want %v", reason, DiscQuitting)
+			}
+		case <-time.After(500 * time.Millisecond):
+			t.Error("peer did not return")
+		}
+	}
+}
+
+// This test is supposed to verify that Peer can reliably handle
+// multiple causes of disconnection occurring at the same time.
+func TestMultiChannelPeerDisconnectRace(t *testing.T) {
+	maybe := func() bool { return rand.Intn(2) == 1 }
+	channelSize := 2
+
+	for i := 0; i < 1000; i++ {
+		protoclose := make(chan error)
+		protodisc := make(chan DiscReason)
+		closer, rws, p, disc := testPeerWithRWs([]Protocol{
+			{
+				Name:       "closereq",
+				RunWithRWs: func(p *Peer, rw []MsgReadWriter) error { return <-protoclose },
+				Length:     1,
+			},
+			{
+				Name:       "disconnect",
+				RunWithRWs: func(p *Peer, rw []MsgReadWriter) error { p.Disconnect(<-protodisc); return nil },
+				Length:     1,
+			},
+		}, channelSize)
+
+		// Simulate incoming messages.
+		for _, rw := range rws {
+			go SendItems(rw, baseProtocolLength+1)
+			go SendItems(rw, baseProtocolLength+2)
+		}
+		// Close the network connection.
+		go closer()
+		// Make protocol "closereq" return.
+		protoclose <- errors.New("protocol closed")
+		// Make protocol "disconnect" call peer.Disconnect
+		protodisc <- DiscAlreadyConnected
+		// In some cases, simulate something else calling peer.Disconnect.
+		if maybe() {
+			go p.Disconnect(DiscInvalidIdentity)
+		}
+		// In some cases, simulate remote requesting a disconnect.
+		if maybe() {
+			go SendItems(rws[rand.Intn(channelSize)], discMsg, DiscQuitting)
 		}
 
 		select {
