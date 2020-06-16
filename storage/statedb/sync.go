f20516bf996ddefa81390969504442e6c791dace
Ethan Kim(김경업) <ethan.kim@groundx.xyz>
2020-06-16 13:58:17 +0900

    go fmt

--- storage/statedb/sync.go
+++ storage/statedb/sync.go
@@ -97,7 +97,7 @@ type TrieSync struct {
 
 // NewTrieSync creates a new trie data download scheduler.
 func NewTrieSync(root common.Hash, database StateTrieReadDB, callback LeafCallback, bloom *SyncBloom) *TrieSync {
-	lruCache, _ := lru.New(int(1*units.Giga/common.HashLength))	// 1GB for 31,250,000 common.Hash key values
+	lruCache, _ := lru.New(int(1 * units.Giga / common.HashLength)) // 1GB for 31,250,000 common.Hash key values
 	ts := &TrieSync{
 		database:         database,
 		membatch:         newSyncMemBatch(),
@@ -106,7 +106,7 @@ func NewTrieSync(root common.Hash, database StateTrieReadDB, callback LeafCallba
 		retrievedByDepth: make(map[int]int),
 		committedByDepth: make(map[int]int),
 		bloom:            bloom,
-		exist: lruCache,
+		exist:            lruCache,
 	}
 	ts.AddSubTrie(root, 0, common.Hash{}, callback)
 	return ts
