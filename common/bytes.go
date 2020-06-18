b170e6bb1979ca79044f024109f36ec96e05b891
winnie.byun <winnie.byun@groundx.xyz>
2020-06-18 16:05:04 +0900

    mv make random byte to common

--- common/bytes.go
+++ common/bytes.go
@@ -23,6 +23,7 @@ package common
 import (
 	"encoding/binary"
 	"encoding/hex"
+	"math/rand"
 )
 
 // ToHex returns the hex representation of b, prefixed with '0x'.
@@ -134,6 +135,12 @@ func LeftPadBytes(slice []byte, l int) []byte {
 	copy(padded[l-len(slice):], slice)
 
 	return padded
+}
+
+func MakeRandomByte(n int) []byte {
+	s := make([]byte, n)
+	rand.Read(s)
+	return s
 }
 
 // IntToByteLittleEndian encodes a number as little endian uint64
