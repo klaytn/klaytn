1b28e838610a44978fe9f8503036225860696735
JK <jk.oh@groundx.xyz>
2020-07-24 13:13:23 +0900

    Reflected the review from Melvin

--- blockchain/vm/memory.go
+++ blockchain/vm/memory.go
@@ -133,6 +133,9 @@ func (m *Memory) Print() {
 }
 
 func (m *Memory) Slice(from, to int64) []byte {
+	if from > int64(m.Len()) {
+		return nil
+	}
 	if to > int64(m.Len()) {
 		to = int64(m.Len())
 	}
