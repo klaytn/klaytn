fdff62531d05ff3a0c66dfd392dc0980dd347604
Aidan <aidan.kwon@groundx.xyz>
2020-09-03 16:33:33 +0900

    Seperate 'cache_hybrid' file from 'cache' file

---
+++ storage/statedb/cache_hybrid.go
@@ -0,0 +1,56 @@
+// Copyright 2020 The klaytn Authors
+// This file is part of the klaytn library.
+//
+// The klaytn library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The klaytn library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
+
+package statedb
+
+func NewHybridCache(maxBytes int, endpoint []string, isCluster bool) (Cache, error) {
+	redis, err := NewRedisCache(endpoint, isCluster)
+	if err != nil {
+		return nil, err
+	}
+	return &hybridCache{
+		local:  NewFastCache(maxBytes),
+		remote: redis,
+	}, nil
+}
+
+// hybridCache integrates two kinds of caches: local, remote.
+// local cache uses memory of the local machine and remote cache uses memory of the remote machine.
+type hybridCache struct {
+	local  Cache
+	remote Cache
+}
+
+func (cache *hybridCache) Set(k, v []byte) {
+	cache.local.Set(k, v)
+	cache.remote.Set(k, v)
+}
+
+func (cache *hybridCache) Get(k []byte) []byte {
+	ret := cache.local.Get(k)
+	if ret != nil {
+		return ret
+	}
+	return cache.remote.Get(k)
+}
+
+func (cache *hybridCache) Has(k []byte) ([]byte, bool) {
+	ret, has := cache.local.Has(k)
+	if has {
+		return ret, has
+	}
+	return cache.remote.Has(k)
+}
--- storage/statedb/cache_test.go
+++ storage/statedb/cache_test.go
@@ -17,17 +17,10 @@
 package statedb
 
 import (
-	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/docker/docker/pkg/testutil/assert"
-)
-
-var (
-	testMaxBytes       = 1024 * 1024
-	testRedisEndpoints = []string{"localhost:6379"}
-	testRedisCluster   = false
 )
 
 // TODO-Klaytn: Enable tests when redis is prepared on CI
@@ -47,130 +40,5 @@ func _TestNewCache(t *testing.T) {
 		cache, err := NewCache(tc.cacheType, testMaxBytes, testRedisEndpoints, testRedisCluster)
 		assert.NilError(t, err)
 		assert.Equal(t, reflect.TypeOf(cache), tc.expectedType)
-	}
-}
-
-// TestHybridCache_Set tests whether a hybrid cache can set an item into both of local and remote caches.
-func _TestHybridCache_Set(t *testing.T) {
-	cache, err := NewHybridCache(testMaxBytes, testRedisEndpoints, testRedisCluster)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Set an item
-	key, value := randBytes(32), randBytes(500)
-	cache.Set(key, value)
-
-	// Type assertion to check both of local cache and remote cache
-	hybrid, ok := cache.(*hybridCache)
-	assert.Equal(t, ok, true)
-
-	// Check whether the item is set in the local cache
-	localVal := hybrid.local.Get(key)
-	assert.Equal(t, bytes.Compare(localVal, value), 0)
-
-	// Check whether the item is set in the remote cache
-	remoteVal := hybrid.remote.Get(key)
-	assert.Equal(t, bytes.Compare(remoteVal, value), 0)
-}
-
-// TestHybridCache_Get tests whether a hybrid cache can get an item from both of local and remote caches.
-func _TestHybridCache_Get(t *testing.T) {
-	// Prepare caches to be integrated with a hybrid cache
-	localCache := NewFastCache(testMaxBytes)
-	remoteCache, err := NewRedisCache(testRedisEndpoints, testRedisCluster)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var hybrid Cache = &hybridCache{
-		local:  localCache,
-		remote: remoteCache,
-	}
-
-	// Test local cache of the hybrid cache
-	{
-		// Store an item into local cache
-		key, value := randBytes(32), randBytes(500)
-		localCache.Set(key, value)
-
-		// Get the item from the hybrid cache and check the validity
-		returnedVal := hybrid.Get(key)
-		assert.Equal(t, bytes.Compare(returnedVal, value), 0)
-	}
-
-	// Test remote cache of the hybrid cache
-	{
-		// Store an item into remote cache
-		key, value := randBytes(32), randBytes(500)
-		remoteCache.Set(key, value)
-
-		// Get the item from the hybrid cache and check the validity
-		returnedVal := hybrid.Get(key)
-		assert.Equal(t, bytes.Compare(returnedVal, value), 0)
-	}
-
-	// Test the priority of local and remote caches
-	{
-		// Store an item into the remote cache
-		key, value := randBytes(32), randBytes(500)
-		localCache.Set(key, value)
-		remoteCache.Set(key, []byte{0x11})
-
-		// Get the item from the hybrid cache and check the validity
-		returnedVal := hybrid.Get(key)
-		assert.Equal(t, bytes.Compare(returnedVal, value), 0)
-	}
-}
-
-// TestHybridCache_Has tests whether a hybrid cache can check an item from both of local and remote caches.
-func _TestHybridCache_Has(t *testing.T) {
-	// Prepare caches to be integrated with a hybrid cache
-	localCache := NewFastCache(testMaxBytes)
-	remoteCache, err := NewRedisCache(testRedisEndpoints, testRedisCluster)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var hybrid Cache = &hybridCache{
-		local:  localCache,
-		remote: remoteCache,
-	}
-
-	// Test local cache of the hybrid cache
-	{
-		// Store an item into local cache
-		key, value := randBytes(32), randBytes(500)
-		localCache.Set(key, value)
-
-		// Get the item from the hybrid cache and check the validity
-		returnedVal, returnedExist := hybrid.Has(key)
-		assert.Equal(t, bytes.Compare(returnedVal, value), 0)
-		assert.Equal(t, returnedExist, true)
-	}
-
-	// Test remote cache of the hybrid cache
-	{
-		// Store an item into remote cache
-		key, value := randBytes(32), randBytes(500)
-		remoteCache.Set(key, value)
-
-		// Get the item from the hybrid cache and check the validity
-		returnedVal, returnedExist := hybrid.Has(key)
-		assert.Equal(t, bytes.Compare(returnedVal, value), 0)
-		assert.Equal(t, returnedExist, true)
-	}
-
-	// Test the priority of local and remote caches
-	{
-		// Store an item into the remote cache
-		key, value := randBytes(32), randBytes(500)
-		localCache.Set(key, value)
-		remoteCache.Set(key, []byte{0x11})
-
-		// Get the item from the hybrid cache and check the validity
-		returnedVal, returnedExist := hybrid.Has(key)
-		assert.Equal(t, bytes.Compare(returnedVal, value), 0)
-		assert.Equal(t, returnedExist, true)
 	}
 }
