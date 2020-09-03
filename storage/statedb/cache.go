52e3936f0294e4261a98cc0f114f8725a0ea56b6
Aidan <aidan.kwon@groundx.xyz>
2020-09-03 12:12:30 +0900

    Introduce hybrid cache as stateDB cache

--- storage/statedb/cache.go
+++ storage/statedb/cache.go
@@ -1,8 +1,77 @@
 package statedb
 
+import "errors"
+
+type CacheType string
+
+const (
+	// Available stateDB cache types
+	LocalCache  CacheType = "LocalCache"
+	RemoteCache           = "RemoteCache"
+	HybridCache           = "HybridCache"
+)
+
+var (
+	errNotSupportedCacheType = errors.New("not supported stateDB Cache type")
+)
+
 // Cache interface the cache of stateDB
 type Cache interface {
 	Set(k, v []byte)
 	Get(k []byte) []byte
 	Has(k []byte) ([]byte, bool)
+}
+
+// NewCache creates one type of any supported stateDB caches.
+// TODO-Klaytn: refine input parameters after setting node flags
+func NewCache(cacheType CacheType, maxBytes int, redisEndpoint []string, redisCluster bool) (Cache, error) {
+	switch cacheType {
+	case LocalCache:
+		return NewFastCache(maxBytes), nil
+	case RemoteCache:
+		return NewRedisCache(redisEndpoint, redisCluster)
+	case HybridCache:
+		return NewHybridCache(maxBytes, redisEndpoint, redisCluster)
+	default:
+	}
+	return nil, errNotSupportedCacheType
+}
+
+func NewHybridCache(maxBytes int, endpoint []string, isCluster bool) (Cache, error) {
+	redis, err := NewRedisCache(endpoint, isCluster)
+	if err != nil {
+		return nil, err
+	}
+	return &hybridCache{
+		local:  NewFastCache(maxBytes),
+		remote: redis,
+	}, nil
+}
+
+// hybridCache integrates two kinds of caches: local, remote.
+// local cache uses memory of the local machine and remote cache uses memory of the remote machine.
+type hybridCache struct {
+	local  Cache
+	remote Cache
+}
+
+func (cache *hybridCache) Set(k, v []byte) {
+	cache.local.Set(k, v)
+	cache.remote.Set(k, v)
+}
+
+func (cache *hybridCache) Get(k []byte) []byte {
+	ret := cache.local.Get(k)
+	if ret != nil {
+		return ret
+	}
+	return cache.remote.Get(k)
+}
+
+func (cache *hybridCache) Has(k []byte) ([]byte, bool) {
+	ret, has := cache.local.Has(k)
+	if has {
+		return ret, has
+	}
+	return cache.remote.Has(k)
 }
