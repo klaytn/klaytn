23542abc5ce6e5017b6e1c28add83eb0da222f43
Melvin Junhee Woo <melvin.woo@groundx.xyz>
2020-11-23 15:58:23 +0900

    fixed TestDatabase_SecureKey

--- storage/statedb/database_test.go
+++ storage/statedb/database_test.go
@@ -112,13 +112,13 @@ func TestDatabase_Size(t *testing.T) {
 
 func TestDatabase_SecureKey(t *testing.T) {
 	secKey1 := secureKey(childHash)
-	copiedSecKey := make([]byte, 0, len(secKey1))
+	copiedSecKey := make([]byte, len(secKey1))
 	copy(copiedSecKey, secKey1)
 
 	secKey2 := secureKey(parentHash)
 
-	assert.NotEqual(t, secKey1, copiedSecKey) // after the next call of secureKey, secKey1 became different from the copied
-	assert.Equal(t, secKey1, secKey2)         // secKey1 has changed into secKey2 as they are created from the same buffer
+	assert.Equal(t, secKey1, copiedSecKey) // after the next call of secureKey, secKey1 became different from the copied
+	assert.NotEqual(t, secKey1, secKey2)   // secKey1 has changed into secKey2 as they are created from the different buffer
 }
 
 func TestCache(t *testing.T) {
