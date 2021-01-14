cd07b1569c21f7572e297579a341395e69d63b75
winnie.byun <winnie.byun@groundx.xyz>
2021-01-14 12:08:38 +0900

    prque with []byte priority

---
+++ common/prque/prque_byteSlice.go
@@ -0,0 +1,72 @@
+// CookieJar - A contestant's algorithm toolbox
+// Copyright (c) 2013 Peter Szilagyi. All rights reserved.
+//
+// CookieJar is dual licensed: use of this source code is governed by a BSD
+// license that can be found in the LICENSE file. Alternatively, the CookieJar
+// toolbox may be used in accordance with the terms and conditions contained
+// in a signed written agreement between you and the author(s).
+
+// This is a duplicated and slightly modified version of "gopkg.in/karalabe/cookiejar.v2/collections/prque".
+
+// Package prque implements a priority queue data structure supporting arbitrary
+// value types and int64 priorities.
+//
+// If you would like to use a max-priority queue, use NewByteSlice.
+// If you would like to use a min-priority queue, use NewByteSliceInverted.
+//
+// Internally the queue is based on the standard heap package working on a
+// sortable version of the block based stack.
+
+package prque
+
+import "container/heap"
+
+type PrqueByteSlice struct {
+	cont *sstackByte
+}
+
+// NewByteSlice creates a new max-priority queue.
+func NewByteSlice() *PrqueByteSlice {
+	return &PrqueByteSlice{newsstackByte(false)}
+}
+
+// NewByteSliceInverted creates a new min-priority queue.
+func NewByteSliceInverted() *PrqueByteSlice {
+	return &PrqueByteSlice{newsstackByte(true)}
+}
+
+// Pushes a value with a given priority into the queue, expanding if necessary.
+func (p *PrqueByteSlice) Push(data interface{}, priority []byte) {
+	heap.Push(p.cont, &itemByteSlice{data, priority})
+}
+
+// Pops the value with the greates priority off the stack and returns it.
+// Currently no shrinking is done.
+func (p *PrqueByteSlice) Pop() (interface{}, []byte) {
+	item := heap.Pop(p.cont).(*itemByteSlice)
+	return item.value, item.priority
+}
+
+// Pops only the item from the queue, dropping the associated priority value.
+func (p *PrqueByteSlice) PopItem() interface{} {
+	return heap.Pop(p.cont).(*itemByteSlice).value
+}
+
+// Checks whether the priority queue is empty.
+func (p *PrqueByteSlice) Empty() bool {
+	return p.cont.Len() == 0
+}
+
+// Returns the number of element in the priority queue.
+func (p *PrqueByteSlice) Size() int {
+	return p.cont.Len()
+}
+
+// Clears the contents of the priority queue.
+func (p *PrqueByteSlice) Reset() {
+	if p.cont.reverse {
+		*p = *NewByteSlice()
+	} else {
+		*p = *NewByteSliceInverted()
+	}
+}
---
+++ common/prque/prque_byteSlice_test.go
@@ -0,0 +1,92 @@
+// CookieJar - A contestant's algorithm toolbox
+// Copyright (c) 2013 Peter Szilagyi. All rights reserved.
+//
+// CookieJar is dual licensed: use of this source code is governed by a BSD
+// license that can be found in the LICENSE file. Alternatively, the CookieJar
+// toolbox may be used in accordance with the terms and conditions contained
+// in a signed written agreement between you and the author(s).
+
+package prque
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/klaytn/klaytn/common/math"
+
+	"github.com/klaytn/klaytn/common"
+)
+
+func TestPrqueByteSlice(t *testing.T) {
+	// Generate a batch of random data and a specific priority order
+	size := 16 * blockSize
+	data := make([]int, size)
+	prio := make([][]byte, size)
+	for i := 0; i < size; i++ {
+		data[i] = rand.Int()
+		prio[i] = common.MakeRandomBytes(256)
+	}
+	queue := NewByteSlice()
+	for rep := 0; rep < 2; rep++ {
+		// Fill a priority queue with the above data
+		for i := 0; i < size; i++ {
+			queue.Push(data[i], prio[i])
+			if queue.Size() != i+1 {
+				t.Errorf("queue size mismatch: have %v, want %v.", queue.Size(), i+1)
+			}
+		}
+		// Create a map the values to the priorities for easier verification
+		dict := make(map[uint64]int)
+		for i := 0; i < size; i++ {
+			dict[common.ByteBigEndianToInt64(prio[i])] = data[i]
+		}
+		// Pop out the elements in priority order and verify them
+		prevPrio := uint64(math.MaxUint64)
+		for !queue.Empty() {
+			val, prio := queue.Pop()
+			if common.ByteBigEndianToInt64(prio) > prevPrio {
+				t.Errorf("invalid priority order: %v after %v.", prio, prevPrio)
+			}
+			prevPrio = common.ByteBigEndianToInt64(prio)
+			if val != dict[prevPrio] {
+				t.Errorf("push/pop mismatch: have %v, want %v.", val, dict[prevPrio])
+			}
+			delete(dict, prevPrio)
+		}
+	}
+}
+
+func BenchmarkPrqueByteSlicePush(b *testing.B) {
+	// Create some initial data
+	data := make([]int, b.N)
+	prio := make([][]byte, b.N)
+	for i := 0; i < len(data); i++ {
+		data[i] = rand.Int()
+		prio[i] = common.MakeRandomBytes(256)
+	}
+	// Execute the benchmark
+	b.ResetTimer()
+	queue := NewByteSlice()
+	for i := 0; i < len(data); i++ {
+		queue.Push(data[i], prio[i])
+	}
+}
+
+func BenchmarkPrqueByteSlicePop(b *testing.B) {
+	// Create some initial data
+	data := make([]int, b.N)
+	prio := make([][]byte, b.N)
+	for i := 0; i < len(data); i++ {
+		data[i] = rand.Int()
+		prio[i] = common.MakeRandomBytes(256)
+	}
+	queue := NewByteSlice()
+	for i := 0; i < len(data); i++ {
+		queue.Push(data[i], prio[i])
+	}
+	// Execute the benchmark
+	b.ResetTimer()
+	for !queue.Empty() {
+		queue.Pop()
+	}
+}
