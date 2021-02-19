f3b0466888e4df6d1217b049bcafa9dba6be0da1
Ethan (Kyungup Kim) <ethan.kim@groundx.xyz>
2021-02-19 19:19:29 +0900

    Update cache.go

--- storage/statedb/cache.go
+++ storage/statedb/cache.go
@@ -30,7 +30,7 @@ type TrieNodeCacheType string
 type TrieNodeCacheConfig struct {
 	CacheType                 TrieNodeCacheType
 	NumFetcherPrefetchWorker  int           // Number of workers used to prefetch a block when fetcher works
-	LocalCacheSizeMiB         int           // Memory allowance (MB) to use for caching trie nodes in fast cache
+	LocalCacheSizeMiB         int           // Memory allowance (MiB) to use for caching trie nodes in fast cache
 	FastCacheFileDir          string        // Directory where the persistent fastcache data is stored
 	FastCacheSavePeriod       time.Duration // Period of saving in memory trie cache to file if fastcache is used
 	RedisEndpoints            []string      // Endpoints of redis cache
