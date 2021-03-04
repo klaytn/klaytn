53db9b5e764b967d8c88753d58e941bd1675db28
Aidan <aidan.kwon@groundx.xyz>
2021-03-04 16:07:01 +0900

    Lower the log level of some consensus msg from WARN to INFO

--- consensus/istanbul/core/roundchange.go
+++ consensus/istanbul/core/roundchange.go
@@ -43,7 +43,8 @@ func (c *core) sendRoundChange(round *big.Int) {
 
 	cv := c.currentView()
 	if cv.Round.Cmp(round) >= 0 {
-		logger.Warn("Skip sending out the round change message", "current round", cv.Round, "target round", round)
+		logger.Warn("[RC] Skip sending out the round change message", "current round", cv.Round,
+			"target round", round)
 		return
 	}
 
