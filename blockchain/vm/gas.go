ebace1314b1c6c676d00f5a0531676a6130de917
yumiel <yumiel.ko@groundx.xyz>
2021-03-05 16:21:16 +0900

    remove unnecessary logic in callGas func

--- blockchain/vm/gas.go
+++ blockchain/vm/gas.go
@@ -38,18 +38,14 @@ const (
 // calcGas returns the actual gas cost of the call.
 //
 // The returned gas is gas - base * 63 / 64.
-
 func callGas(availableGas, base uint64, callCost *big.Int) (uint64, error) {
 	availableGas = availableGas - base
 	gas := availableGas - availableGas/64
 	// If the bit length exceeds 64 bit we know that the newly calculated "gas" for EIP150
-	// is smaller than the requested amount. Therefor we return the new gas instead
+	// is smaller than the requested amount. Therefore we return the new gas instead
 	// of returning an error.
 	if !callCost.IsUint64() || gas < callCost.Uint64() {
 		return gas, nil
-	}
-	if !callCost.IsUint64() {
-		return 0, errGasUintOverflow
 	}
 
 	return callCost.Uint64(), nil
