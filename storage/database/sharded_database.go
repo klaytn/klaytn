2737d9b4116098dc2e9986aced923caf6fdc0916
Ethan (Kyungup Kim) <ethan.kim@groundx.xyz>
2021-05-04 22:49:22 +0900

    seperate function and adjust max shard value and align quit signal routine

--- storage/database/sharded_database.go
+++ storage/database/sharded_database.go
@@ -52,7 +52,7 @@ type sdbBatchResult struct {
 // newShardedDB creates database with numShards shards, or partitions.
 // The type of database is specified DBConfig.DBType.
 func newShardedDB(dbc *DBConfig, et DBEntryType, numShards uint) (*shardedDB, error) {
-	const numShardsLimit = 16
+	const numShardsLimit = 256
 
 	if numShards == 0 {
 		logger.Crit("numShards should be greater than 0!")
