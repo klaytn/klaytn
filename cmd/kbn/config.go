22a1e35dea4c6bdb334117f98098e5db857bd3e3
hyochan-brown <brown.kim@groundx.xyz>
2021-07-21 13:29:12 +0900

    refactor: Make duplicated code to funtion in legacy flag check

--- cmd/kbn/config.go
+++ cmd/kbn/config.go
@@ -219,50 +219,27 @@ func setAuthorizedNodes(ctx *cli.Context, cfg *bootnodeConfig) {
 // setHTTP creates the HTTP RPC listener interface string from the set
 // command line flags, returning empty if the HTTP endpoint is disabled.
 func setHTTP(ctx *cli.Context, cfg *bootnodeConfig) {
-	if ctx.GlobalBool(utils.HTTPEnabledFlag.Name) && cfg.HTTPHost == "" {
+	if ctx.GlobalBool(getFlagName(ctx, utils.HTTPEnabledFlag, utils.RPCEnabledFlag, "(v1.12.0)")) && cfg.HTTPHost == "" {
 		cfg.HTTPHost = "127.0.0.1"
-		if ctx.GlobalIsSet(utils.HTTPListenAddrFlag.Name) {
-			cfg.HTTPHost = ctx.GlobalString(utils.HTTPListenAddrFlag.Name)
-		}
-	} else if ctx.GlobalBool(utils.RPCEnabledFlag.Name) && cfg.HTTPHost == "" {
-		cfg.HTTPHost = "127.0.0.1"
-		logger.Warn("The flag --rpc is deprecated and will be removed in the future (v1.12.0), please use --http")
-		if ctx.GlobalIsSet(utils.RPCListenAddrFlag.Name) {
-			cfg.HTTPHost = ctx.GlobalString(utils.RPCListenAddrFlag.Name)
-			logger.Warn("The flag --rpcaddr is deprecated and will be removed in the future (v1.12.0), please use --http.addr")
+		if listenAddrFlagName := getFlagName(ctx, utils.HTTPListenAddrFlag, utils.RPCListenAddrFlag, "(v1.12.0)"); ctx.GlobalIsSet(listenAddrFlagName) {
+			cfg.HTTPHost = ctx.GlobalString(listenAddrFlagName)
 		}
 	}
 
-	if ctx.GlobalIsSet(utils.HTTPPortFlag.Name) {
-		cfg.HTTPPort = ctx.GlobalInt(utils.HTTPPortFlag.Name)
-	} else if ctx.GlobalIsSet(utils.RPCPortFlag.Name) {
-		cfg.HTTPPort = ctx.GlobalInt(utils.RPCPortFlag.Name)
-		logger.Warn("The flag --rpcport is deprecated and will be removed in the future (v1.12.0), please use --http.port")
+	if portFlagName := getFlagName(ctx, utils.HTTPPortFlag, utils.RPCPortFlag, "(v1.12.0)"); ctx.GlobalIsSet(portFlagName) {
+		cfg.HTTPPort = ctx.GlobalInt(portFlagName)
 	}
-	if ctx.GlobalIsSet(utils.HTTPCORSDomainFlag.Name) {
-		cfg.HTTPCors = splitAndTrim(ctx.GlobalString(utils.HTTPCORSDomainFlag.Name))
-	} else if ctx.GlobalIsSet(utils.RPCCORSDomainFlag.Name) {
-		cfg.HTTPCors = splitAndTrim(ctx.GlobalString(utils.RPCCORSDomainFlag.Name))
-		logger.Warn("The flag --rpccorsdomain is deprecated and will be removed in the future (v1.12.0), please use --http.corsdomain")
+	if corsDomainFlagName := getFlagName(ctx, utils.HTTPCORSDomainFlag, utils.RPCCORSDomainFlag, "(v1.12.0)"); ctx.GlobalIsSet(corsDomainFlagName) {
+		cfg.HTTPCors = splitAndTrim(ctx.GlobalString(corsDomainFlagName))
 	}
-	if ctx.GlobalIsSet(utils.HTTPApiFlag.Name) {
-		cfg.HTTPModules = splitAndTrim(ctx.GlobalString(utils.HTTPApiFlag.Name))
-	} else if ctx.GlobalIsSet(utils.RPCApiFlag.Name) {
-		cfg.HTTPModules = splitAndTrim(ctx.GlobalString(utils.RPCApiFlag.Name))
-		logger.Warn("The flag --rpcapi is deprecated and will be removed in the future (v1.12.0), please use --http.api")
+	if apiFlagName := getFlagName(ctx, utils.HTTPApiFlag, utils.RPCApiFlag, "(v1.12.0)"); ctx.GlobalIsSet(apiFlagName) {
+		cfg.HTTPModules = splitAndTrim(ctx.GlobalString(apiFlagName))
 	}
-	if ctx.GlobalIsSet(utils.HTTPVirtualHostsFlag.Name) {
-		cfg.HTTPVirtualHosts = splitAndTrim(ctx.GlobalString(utils.HTTPVirtualHostsFlag.Name))
-	} else if ctx.GlobalIsSet(utils.RPCVirtualHostsFlag.Name) {
-		cfg.HTTPVirtualHosts = splitAndTrim(ctx.GlobalString(utils.RPCVirtualHostsFlag.Name))
-		logger.Warn("The flag --rpcvhosts is deprecated and will be removed in the future (v1.12.0), please use --http.vhosts")
+	if virtualHostsFlagName := getFlagName(ctx, utils.HTTPVirtualHostsFlag, utils.RPCVirtualHostsFlag, "(v1.12.0)"); ctx.GlobalIsSet(virtualHostsFlagName) {
+		cfg.HTTPVirtualHosts = splitAndTrim(ctx.GlobalString(virtualHostsFlagName))
 	}
-	if ctx.GlobalIsSet(utils.HTTPConcurrencyLimit.Name) {
-		rpc.ConcurrencyLimit = ctx.GlobalInt(utils.HTTPConcurrencyLimit.Name)
-		logger.Info("Set the concurrency limit of RPC-HTTP server", "limit", rpc.ConcurrencyLimit)
-	} else if ctx.GlobalIsSet(utils.RPCConcurrencyLimit.Name) {
-		rpc.ConcurrencyLimit = ctx.GlobalInt(utils.RPCConcurrencyLimit.Name)
-		logger.Warn("The flag --rpcconcurrencylimit is deprecated and will be removed in the future (v1.12.0), please use --http.concurrencylimit")
+	if concurrencyLimitFlagName := getFlagName(ctx, utils.HTTPConcurrencyLimit, utils.RPCConcurrencyLimit, "(v1.12.0)"); ctx.GlobalIsSet(concurrencyLimitFlagName) {
+		rpc.ConcurrencyLimit = ctx.GlobalInt(concurrencyLimitFlagName)
 		logger.Info("Set the concurrency limit of RPC-HTTP server", "limit", rpc.ConcurrencyLimit)
 	}
 }
@@ -271,32 +248,20 @@ func setHTTP(ctx *cli.Context, cfg *bootnodeConfig) {
 // command line flags, returning empty if the HTTP endpoint is disabled.
 func setWS(ctx *cli.Context, cfg *bootnodeConfig) {
 	if ctx.GlobalBool(utils.WSEnabledFlag.Name) && cfg.WSHost == "" {
-		cfg.WSHost = "127.0.0.1"
-		if ctx.GlobalIsSet(utils.WSListenAddrFlag.Name) {
-			cfg.WSHost = ctx.GlobalString(utils.WSListenAddrFlag.Name)
-		} else if ctx.GlobalIsSet(utils.LegacyWSListenAddrFlag.Name) {
-			cfg.WSHost = ctx.GlobalString(utils.LegacyWSListenAddrFlag.Name)
-			logger.Warn("The flag --wsaddr is deprecated and will be removed in the future (v1.12.0), please use --ws.addr")
+		cfg.HTTPHost = "127.0.0.1"
+		if listenAddrFlagName := getFlagName(ctx, utils.WSListenAddrFlag, utils.LegacyWSListenAddrFlag, "(v1.12.0)"); ctx.GlobalIsSet(listenAddrFlagName) {
+			cfg.WSHost = ctx.GlobalString(listenAddrFlagName)
 		}
 	}
 
-	if ctx.GlobalIsSet(utils.WSPortFlag.Name) {
-		cfg.WSPort = ctx.GlobalInt(utils.WSPortFlag.Name)
-	} else if ctx.GlobalIsSet(utils.LegacyWSPortFlag.Name) {
-		cfg.WSPort = ctx.GlobalInt(utils.LegacyWSPortFlag.Name)
-		logger.Warn("The flag --wsport is deprecated and will be removed in the future (v1.12.0), please use --ws.port")
+	if wsPortFlagName := getFlagName(ctx, utils.WSPortFlag, utils.LegacyWSPortFlag, "(v1.12.0)"); ctx.GlobalIsSet(wsPortFlagName) {
+		cfg.WSPort = ctx.GlobalInt(wsPortFlagName)
 	}
-	if ctx.GlobalIsSet(utils.WSAllowedOriginsFlag.Name) {
-		cfg.WSOrigins = splitAndTrim(ctx.GlobalString(utils.WSAllowedOriginsFlag.Name))
-	} else if ctx.GlobalIsSet(utils.LegacyWSAllowedOriginsFlag.Name) {
-		cfg.WSOrigins = splitAndTrim(ctx.GlobalString(utils.LegacyWSAllowedOriginsFlag.Name))
-		logger.Warn("The flag --wsorigins is deprecated and will be removed in the future (v1.12.0), please use --ws.origins")
+	if wsAllowOriginsFlagName := getFlagName(ctx, utils.WSAllowedOriginsFlag, utils.LegacyWSAllowedOriginsFlag, "(v1.12.0)"); ctx.GlobalIsSet(wsAllowOriginsFlagName) {
+		cfg.WSOrigins = splitAndTrim(ctx.GlobalString(wsAllowOriginsFlagName))
 	}
-	if ctx.GlobalIsSet(utils.WSApiFlag.Name) {
-		cfg.WSModules = splitAndTrim(ctx.GlobalString(utils.WSApiFlag.Name))
-	} else if ctx.GlobalIsSet(utils.LegacyWSApiFlag.Name) {
-		cfg.WSModules = splitAndTrim(ctx.GlobalString(utils.LegacyWSApiFlag.Name))
-		logger.Warn("The flag --wsapi is deprecated and will be removed in the future (v1.12.0), please use --ws.api")
+	if wsApiFlagName := getFlagName(ctx, utils.WSApiFlag, utils.LegacyWSApiFlag, "(v1.12.0)"); ctx.GlobalIsSet(wsApiFlagName) {
+		cfg.WSModules = splitAndTrim(ctx.GlobalString(wsApiFlagName))
 	}
 }
 
@@ -317,17 +282,12 @@ func setIPC(ctx *cli.Context, cfg *bootnodeConfig) {
 func setgRPC(ctx *cli.Context, cfg *bootnodeConfig) {
 	if ctx.GlobalBool(utils.GRPCEnabledFlag.Name) && cfg.GRPCHost == "" {
 		cfg.GRPCHost = "127.0.0.1"
-		if ctx.GlobalIsSet(utils.GRPCListenAddrFlag.Name) {
-			cfg.GRPCHost = ctx.GlobalString(utils.GRPCListenAddrFlag.Name)
-		} else if ctx.GlobalIsSet(utils.LegacyGRPCListenAddrFlag.Name) {
-			cfg.GRPCHost = ctx.GlobalString(utils.LegacyGRPCListenAddrFlag.Name)
+		if grpcHostName := getFlagName(ctx, utils.GRPCListenAddrFlag, utils.LegacyGRPCListenAddrFlag, "(v1.12.0)"); ctx.GlobalIsSet(grpcHostName) {
+			cfg.GRPCHost = ctx.GlobalString(grpcHostName)
 		}
 	}
-
-	if ctx.GlobalIsSet(utils.GRPCPortFlag.Name) {
-		cfg.GRPCPort = ctx.GlobalInt(utils.GRPCPortFlag.Name)
-	} else if ctx.GlobalIsSet(utils.LegacyGRPCPortFlag.Name) {
-		cfg.GRPCPort = ctx.GlobalInt(utils.LegacyGRPCPortFlag.Name)
+	if grpcPortName := getFlagName(ctx, utils.GRPCPortFlag, utils.LegacyGRPCPortFlag, "(v1.12.0)"); ctx.GlobalIsSet(grpcPortName) {
+		cfg.GRPCHost = ctx.GlobalString(grpcPortName)
 	}
 }
 
@@ -495,4 +455,15 @@ func (c *bootnodeConfig) GRPCEndpoint() string {
 func DefaultGRPCEndpoint() string {
 	config := &bootnodeConfig{GRPCHost: DefaultGRPCHost, GRPCPort: DefaultGRPCPort}
 	return config.GRPCEndpoint()
+}
+
+func getFlagName(ctx *cli.Context, flag cli.Flag, legacyFlag cli.Flag, deprecateVersion string) string {
+	if ctx.GlobalIsSet(flag.GetName()) {
+		return flag.GetName()
+	}
+	if ctx.GlobalIsSet(legacyFlag.GetName()) {
+		logger.Warn("The flag " + legacyFlag.GetName() + " is deprecated and will be removed in the future " + deprecateVersion + ", please use " + flag.GetName())
+		return legacyFlag.GetName()
+	}
+	return ""
 }
