11e3a3e677ede4e22374bc6178d6e14010b9cf43
Denver <aeharvlee@gmail.com>
2022-02-10 08:49:08 +0900

    Added license statements

--- node/cn/gasprice/feehistory.go
+++ node/cn/gasprice/feehistory.go
@@ -1,3 +1,23 @@
+// Modifications Copyright 2022 The Klaytn Authors
+// Copyright 2021 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is derived from eth/gasprice/feehistory.go (2021/11/09).
+// Modified and improved for the klaytn development.
+
 package gasprice
 
 import (
