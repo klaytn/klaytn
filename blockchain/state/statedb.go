55b832f760f17c68c4f595c7d6feffbd7d7437ac
Denver <aeharvlee@gmail.com>
2022-02-10 16:39:30 +0900

    Add comment to SetStorage

--- blockchain/state/statedb.go
+++ blockchain/state/statedb.go
@@ -471,6 +471,8 @@ func (self *StateDB) SetState(addr common.Address, key, value common.Hash) {
 	}
 }
 
+// SetStorage replaces the entire storage for the specified account with given
+// storage. This function should only be used for debugging.
 func (self *StateDB) SetStorage(addr common.Address, storage map[common.Hash]common.Hash) {
 	stateObject := self.GetOrNewStateObject(addr)
 	if stateObject != nil {
