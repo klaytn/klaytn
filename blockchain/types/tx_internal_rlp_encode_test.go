d0e9ff56e5390d561666796c3bdee975bafd03a8
kale <kale.krust@krustuniverse.com>
2022-02-14 09:16:35 +0900

    Update blockchain/types/tx_internal_rlp_encode_test.go
    Co-authored-by: Jamie <32922423+jimni1222@users.noreply.github.com>

--- blockchain/types/tx_internal_rlp_encode_test.go
+++ blockchain/types/tx_internal_rlp_encode_test.go
@@ -223,7 +223,6 @@ func testTxRLPEncodeAccessList(t *testing.T) {
 }
 
 func testTxRLPEncodeDynamicFee(t *testing.T) {
-	//prvKey, _:= crypto.HexToECDSA("0cfd086137699e1371a78e648748be0011de423269805c28d2d7b9973dcdb3ad")
 	tx := genDynamicFeeTransaction().(*TxInternalDataDynamicFee)
 
 	signer := LatestSignerForChainID(big.NewInt(2))
