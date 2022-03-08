8cb97463420fdd4d6fd2d9a97b1a0e2766c52967
hqjang-pepper <pepper.jang@groundx.xyz>
2022-03-08 15:55:29 +0900

    fix dbmanager problem

--- storage/database/db_manager.go
+++ storage/database/db_manager.go
@@ -2292,13 +2292,13 @@ func (dbm *databaseManager) DeleteGovernanceIdx(blockNum uint64) error {
 
 	if idxHistory, err := dbm.ReadRecentGovernanceIdx(0); err != nil {
 		return err
-	}
-
-	for i := 0; i < len(idxHistory); i++ {
-		if idxHistory[i] > blockNum {
-			break
+	} else {
+		for i := 0; i < len(idxHistory); i++ {
+			if idxHistory[i] > blockNum {
+				break
+			}
+			newSlice = append(newSlice, idxHistory[i])
 		}
-		newSlice = append(newSlice, idxHistory[i])
 	}
 
 	data, err := json.Marshal(newSlice)
