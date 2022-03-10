6a7514c86f38f8f5e430cc9d1ae354c39266b984
Denver <aeharvlee@gmail.com>
2022-03-10 17:53:38 +0900

    Check args when Ethereum typed transactions before signing.

--- api/api_public_transaction_pool.go
+++ api/api_public_transaction_pool.go
@@ -400,6 +400,12 @@ type SignTransactionResult struct {
 // The node needs to have the private key of the account corresponding with
 // the given from address and it needs to be unlocked.
 func (s *PublicTransactionPoolAPI) SignTransaction(ctx context.Context, args SendTxArgs) (*SignTransactionResult, error) {
+	if args.TypeInt.IsEthTypedTransaction() {
+		if args.Price == nil && (args.MaxPriorityFeePerGas == nil || args.MaxFeePerGas == nil) {
+			return nil, fmt.Errorf("missing gasPrice or maxFeePerGas/maxPriorityFeePerGas")
+		}
+	}
+
 	// No need to obtain the noncelock mutex, since we won't be sending this
 	// tx into the transaction pool, but right back to the user
 	if err := args.setDefaults(ctx, s.b); err != nil {
