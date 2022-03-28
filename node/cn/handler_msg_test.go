8a49c264d85223b61cbaaa781402d87f0ce20a40
kale <kale.krust@krustuniverse.com>
2022-03-28 16:02:43 +0900

    Modified test case : The time field in transaction can be different.

--- node/cn/handler_msg_test.go
+++ node/cn/handler_msg_test.go
@@ -260,9 +260,13 @@ func TestHandleTxMsg(t *testing.T) {
 	}
 	// If pm.acceptTxs == 1, TxPool.HandleTxMsg is called.
 	{
+
 		atomic.StoreUint32(&pm.acceptTxs, 1)
 		mockTxPool := mocks.NewMockTxPool(mockCtrl)
-		mockTxPool.EXPECT().HandleTxMsg(gomock.Eq(txs)).AnyTimes()
+
+		//The time field in received transaction through pm.handleMsg() has different value from generated transaction(`tx1`).
+		//It can check whether the transaction created `HandleTxMsg()` is the same as `tx1` through `AddToKnownTxs(txs[0].Hash())`.
+		mockTxPool.EXPECT().HandleTxMsg(gomock.Any()).AnyTimes()
 		pm.txpool = mockTxPool
 
 		mockPeer.EXPECT().AddToKnownTxs(txs[0].Hash()).Times(1)
