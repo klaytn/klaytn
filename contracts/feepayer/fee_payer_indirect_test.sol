3a5a98414fdf466aa986215e158629a6405e09b4
Aidan <aidan.kwon@krustuniverse.com>
2022-04-12 20:45:21 +0900

    Change solidity version pragma of test contracts

--- contracts/feepayer/fee_payer_indirect_test.sol
+++ contracts/feepayer/fee_payer_indirect_test.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.11;
+pragma solidity >0.8.0;
 
 interface FeePayer {
     function GetFeePayer() external returns (address);
