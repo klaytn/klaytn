57d48efc50842ebbef729e97c2b8c9027fe9c437
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2022-04-18 09:23:44 +0900

    [Bridge manager] Recovered Copyright

--- node/sc/bridge_manager.go
+++ node/sc/bridge_manager.go
@@ -12,6 +12,7 @@
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
+// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
 
 package sc
 
