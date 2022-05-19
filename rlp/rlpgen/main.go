7dc91043c37f8e8fc979e12a5b3a9477cc7e2a9f
kale <kale.krust@krustuniverse.com>
2022-05-19 11:12:56 +0900

    Modified file header.

--- rlp/rlpgen/main.go
+++ rlp/rlpgen/main.go
@@ -1,3 +1,4 @@
+// Modifications Copyright 2022 The klaytn Authors
 // Copyright 2021 The go-ethereum Authors
 // This file is part of the go-ethereum library.
 //
@@ -13,6 +14,9 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is derived from rlp/rlpgen/main.go(2022/05/19)
+// Modified and improved for the klaytn development.
 
 package main
 
