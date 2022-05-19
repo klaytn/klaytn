862e9db1a51788f196800c8a278ed378246c497f
kale <kale.krust@krustuniverse.com>
2022-05-19 11:12:42 +0900

    modified pathOfPackageRLP

--- rlp/rlpgen/main.go
+++ rlp/rlpgen/main.go
@@ -28,7 +28,7 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
-const pathOfPackageRLP = "github.com/ethereum/go-ethereum/rlp"
+const pathOfPackageRLP = "github.com/klaytn/klaytn/rlp"
 
 func main() {
 	var (
