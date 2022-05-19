3c32ae7d7eb148c69ead312584b225550f49a7cd
kale <kale.krust@krustuniverse.com>
2022-05-19 16:40:46 +0900

    apply sorting TxByTime

--- node/cn/peer.go
+++ node/cn/peer.go
@@ -433,8 +433,8 @@ func (p *basePeer) Send(msgcode uint64, data interface{}) error {
 // in its transaction hash set for future reference.
 func (p *basePeer) SendTransactions(txs types.Transactions) error {
 	// Before sending transactions, sort transactions in ascending order by time.
-	if !sort.IsSorted(types.TxByPriceAndTime(txs)) {
-		sort.Sort(types.TxByPriceAndTime(txs))
+	if !sort.IsSorted(types.TxByTime(txs)) {
+		sort.Sort(types.TxByTime(txs))
 	}
 
 	for _, tx := range txs {
@@ -446,8 +446,8 @@ func (p *basePeer) SendTransactions(txs types.Transactions) error {
 // ReSendTransactions sends txs to a peer in order to prevent the txs from missing.
 func (p *basePeer) ReSendTransactions(txs types.Transactions) error {
 	// Before sending transactions, sort transactions in ascending order by time.
-	if !sort.IsSorted(types.TxByPriceAndTime(txs)) {
-		sort.Sort(types.TxByPriceAndTime(txs))
+	if !sort.IsSorted(types.TxByTime(txs)) {
+		sort.Sort(types.TxByTime(txs))
 	}
 
 	return p2p.Send(p.rw, TxMsg, txs)
@@ -812,8 +812,8 @@ func (p *multiChannelPeer) Broadcast() {
 // in its transaction hash set for future reference.
 func (p *multiChannelPeer) SendTransactions(txs types.Transactions) error {
 	// Before sending transactions, sort transactions in ascending order by time.
-	if !sort.IsSorted(types.TxByPriceAndTime(txs)) {
-		sort.Sort(types.TxByPriceAndTime(txs))
+	if !sort.IsSorted(types.TxByTime(txs)) {
+		sort.Sort(types.TxByTime(txs))
 	}
 
 	for _, tx := range txs {
@@ -825,8 +825,8 @@ func (p *multiChannelPeer) SendTransactions(txs types.Transactions) error {
 // ReSendTransactions sends txs to a peer in order to prevent the txs from missing.
 func (p *multiChannelPeer) ReSendTransactions(txs types.Transactions) error {
 	// Before sending transactions, sort transactions in ascending order by time.
-	if !sort.IsSorted(types.TxByPriceAndTime(txs)) {
-		sort.Sort(types.TxByPriceAndTime(txs))
+	if !sort.IsSorted(types.TxByTime(txs)) {
+		sort.Sort(types.TxByTime(txs))
 	}
 
 	return p.msgSender(TxMsg, txs)
