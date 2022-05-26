23503cc915210d19fab45b30f02cce6aebf9c557
jack.jin <jack.jin@krustuniverse.com>
2022-05-26 17:24:32 +0900

    delete Default prefix of hard coded KIP71CompatibleBlockNum

--- params/config.go
+++ params/config.go
@@ -145,7 +145,7 @@ var (
 	VMLogTarget = 0x0
 
 	// TODO-klaytn temporal number for test
-	DefaultKIP71CompatibleBlockNum = big.NewInt(95000000)
+	KIP71CompatibleBlockNum = big.NewInt(95000000)
 )
 
 const (
