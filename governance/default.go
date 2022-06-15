953baedc473a327725e2c12e30d3228491befe9f
Yunjong Jeong <yunjong@kaist.ac.kr>
2022-06-15 18:03:38 +0900

    Remove unused getters

--- governance/default.go
+++ governance/default.go
@@ -1109,64 +1109,6 @@ func AddGovernanceCacheForTest(e HeaderEngine, num uint64, config *params.ChainC
 	}
 }
 
-// TODO-Klaytn: Remove nominal getters once we add Governnace.Params() and UpdateParams()
-
-func (gov *Governance) GovernanceMode() string {
-	return gov.GetGovernanceValue(params.GovernanceMode).(string)
-}
-
-func (gov *Governance) GoverningNode() common.Address {
-	return gov.GetGovernanceValue(params.GoverningNode).(common.Address)
-}
-
-func (gov *Governance) UnitPrice() uint64 {
-	return gov.GetGovernanceValue(params.UnitPrice).(uint64)
-}
-
-func (gov *Governance) CommitteeSize() uint64 {
-	return gov.GetGovernanceValue(params.CommitteeSize).(uint64)
-}
-
-func (gov *Governance) Epoch() uint64 {
-	if ret := gov.GetGovernanceValue(params.Epoch); ret == nil {
-		// When a node is initializing, value can be nil
-		return gov.ChainConfig.Istanbul.Epoch
-	}
-	return gov.GetGovernanceValue(params.Epoch).(uint64)
-}
-
-func (gov *Governance) ProposerPolicy() uint64 {
-	return gov.GetGovernanceValue(params.Policy).(uint64)
-}
-
-func (gov *Governance) DeferredTxFee() bool {
-	return gov.GetGovernanceValue(params.DeferredTxFee).(bool)
-}
-
-func (gov *Governance) MinimumStake() string {
-	return gov.GetGovernanceValue(params.MinimumStake).(string)
-}
-
-func (gov *Governance) MintingAmount() string {
-	return gov.GetGovernanceValue(params.MintingAmount).(string)
-}
-
-func (gov *Governance) ProposerUpdateInterval() uint64 {
-	return gov.GetGovernanceValue(params.ProposerRefreshInterval).(uint64)
-}
-
-func (gov *Governance) Ratio() string {
-	return gov.GetGovernanceValue(params.Ratio).(string)
-}
-
-func (gov *Governance) StakingUpdateInterval() uint64 {
-	return gov.GetGovernanceValue(params.StakeUpdateInterval).(uint64)
-}
-
-func (gov *Governance) UseGiniCoeff() bool {
-	return gov.GetGovernanceValue(params.UseGiniCoeff).(bool)
-}
-
 func (gov *Governance) ChainId() uint64 {
 	return gov.ChainConfig.ChainID.Uint64()
 }
