c1c5975a7b56ab6c47a19a878d234030616e8825
jack.jin <jack.jin@krustuniverse.com>
2022-07-20 10:32:43 +0900

    adjust logic checking dynamicFee field to do same thing in setDefaults of tx_args.go and api_ethereum.go

--- api/tx_args.go
+++ api/tx_args.go
@@ -185,10 +185,8 @@ func (args *SendTxArgs) setDefaults(ctx context.Context, b Backend) error {
 			}
 			args.MaxFeePerGas = (*hexutil.Big)(gasFeeCap)
 		}
-		if isMagma {
-			if args.MaxFeePerGas.ToInt().Cmp(new(big.Int).Div(gasPrice, common.Big2)) < 0 {
-				return fmt.Errorf("maxFeePerGas (%v) < BaseFee (%v)", args.MaxFeePerGas, gasPrice)
-			}
+		if isMagma && args.MaxFeePerGas.ToInt().Cmp(new(big.Int).Div(gasPrice, common.Big2)) < 0 {
+			return fmt.Errorf("maxFeePerGas (%v) < BaseFee (%v)", args.MaxFeePerGas, gasPrice)
 		} else if args.MaxPriorityFeePerGas.ToInt().Cmp(gasPrice) != 0 || args.MaxFeePerGas.ToInt().Cmp(gasPrice) != 0 {
 			return fmt.Errorf("only %s is allowed to be used as maxFeePerGas and maxPriorityPerGas", gasPrice.Text(16))
 		}
