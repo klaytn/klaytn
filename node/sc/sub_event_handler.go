880d1aeab6d087997958f4c85bf853177b624e36
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2022-08-01 19:51:32 +0900

    Apply go module for looking up parentOperatorBalance

--- node/sc/sub_event_handler.go
+++ node/sc/sub_event_handler.go
@@ -45,17 +45,19 @@ func (cce *ChildChainEventHandler) HandleChainHeadEvent(block *types.Block) erro
 	logger.Trace("bridgeNode block number", "number", block.Number())
 	cce.handler.LocalChainHeadEvent(block)
 
-	pBalance, _p := cce.handler.getParentOperatorBalance()
-	if _p != nil {
-		logger.Info("can't check parentOperatorBalance info", "_p", _p)
-	} else {
-		logger.Info("parentOperator", "address", cce.handler.GetParentOperatorAddr(), "balance", pBalance.Int64())
-		parentOperatorBalanceGauge.Update(pBalance.Int64())
-	}
+	go func() {
+		pBalance, err := cce.handler.getParentOperatorBalance()
+		if err != nil {
+			logger.Info("can't check parentOperatorBalance info", "_p", err)
+		} else {
+			logger.Info("parentOperator", "address", cce.handler.GetParentOperatorAddr(), "balance", pBalance.Int64())
+			parentOperatorBalanceGauge.Update(pBalance.Int64())
+		}
+	}()
 
-	cBalance, _c := cce.handler.getChildOperatorBalance()
-	if _c != nil {
-		logger.Info("can't check childOperatorBalance info", "_c", _c)
+	cBalance, err := cce.handler.getChildOperatorBalance()
+	if err != nil {
+		logger.Info("can't check childOperatorBalance info", "err", err)
 	} else {
 		logger.Info("childOperator", "address", cce.handler.GetChildOperatorAddr(), "balance", cBalance.Int64())
 		childOperatorBalanceGauge.Update(cBalance.Int64())
