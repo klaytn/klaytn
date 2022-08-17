e00f6abc5f5cdb3e979578319d1075024592619b
Chihyun Song <ian0371@gmail.com>
2022-08-17 17:11:02 +0900

    Add ChainConfig.Governance.GovParamContract

--- params/config.go
+++ params/config.go
@@ -49,8 +49,9 @@ var (
 		KoreCompatibleBlock:      big.NewInt(0xffffffff),
 		DeriveShaImpl:            2,
 		Governance: &GovernanceConfig{
-			GoverningNode:  common.HexToAddress("0x52d41ca72af615a1ac3301b0a93efa222ecc7541"),
-			GovernanceMode: "single",
+			GoverningNode:    common.HexToAddress("0x52d41ca72af615a1ac3301b0a93efa222ecc7541"),
+			GovernanceMode:   "single",
+			GovParamContract: common.HexToAddress("0x0"),
 			Reward: &RewardConfig{
 				MintingAmount:          mintingAmount,
 				Ratio:                  "34/54/12",
@@ -79,8 +80,9 @@ var (
 		KoreCompatibleBlock:      big.NewInt(0xffffffff),
 		DeriveShaImpl:            2,
 		Governance: &GovernanceConfig{
-			GoverningNode:  common.HexToAddress("0x99fb17d324fa0e07f23b49d09028ac0919414db6"),
-			GovernanceMode: "single",
+			GoverningNode:    common.HexToAddress("0x99fb17d324fa0e07f23b49d09028ac0919414db6"),
+			GovernanceMode:   "single",
+			GovParamContract: common.HexToAddress("0x0"),
 			Reward: &RewardConfig{
 				MintingAmount:          mintingAmount,
 				Ratio:                  "34/54/12",
@@ -189,10 +191,11 @@ type ChainConfig struct {
 
 // GovernanceConfig stores governance information for a network
 type GovernanceConfig struct {
-	GoverningNode  common.Address `json:"governingNode"`
-	GovernanceMode string         `json:"governanceMode"`
-	Reward         *RewardConfig  `json:"reward,omitempty"`
-	KIP71          *KIP71Config   `json:"kip71,omitempty"`
+	GoverningNode    common.Address `json:"governingNode"`
+	GovernanceMode   string         `json:"governanceMode"`
+	GovParamContract common.Address `json:"govParamContract"`
+	Reward           *RewardConfig  `json:"reward,omitempty"`
+	KIP71            *KIP71Config   `json:"kip71,omitempty"`
 }
 
 func (g *GovernanceConfig) DeferredTxFee() bool {
@@ -514,10 +517,11 @@ func (c *ChainConfig) Rules(num *big.Int) Rules {
 
 func GetDefaultGovernanceConfig() *GovernanceConfig {
 	gov := &GovernanceConfig{
-		GovernanceMode: DefaultGovernanceMode,
-		GoverningNode:  common.HexToAddress(DefaultGoverningNode),
-		Reward:         GetDefaultRewardConfig(),
-		KIP71:          GetDefaultKIP71Config(),
+		GovernanceMode:   DefaultGovernanceMode,
+		GoverningNode:    common.HexToAddress(DefaultGoverningNode),
+		GovParamContract: common.HexToAddress(DefaultGovParamContract),
+		Reward:           GetDefaultRewardConfig(),
+		KIP71:            GetDefaultKIP71Config(),
 	}
 	return gov
 }
