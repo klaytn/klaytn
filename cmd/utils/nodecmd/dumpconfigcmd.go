ad9bfd3eda8f5563394b5bf2fcb757059fd00320
jack.jin <jack.jin@krustuniverse.com>
2022-10-17 17:39:24 +0900

    util function CheckExclusive returned to flags.go

--- cmd/utils/nodecmd/dumpconfigcmd.go
+++ cmd/utils/nodecmd/dumpconfigcmd.go
@@ -419,7 +419,7 @@ func setWS(ctx *cli.Context, cfg *node.Config) {
 // setIPC creates an IPC path configuration from the set command line flags,
 // returning an empty string if IPC was explicitly disabled, or the set path.
 func setIPC(ctx *cli.Context, cfg *node.Config) {
-	CheckExclusive(ctx, utils.IPCDisabledFlag, utils.IPCPathFlag)
+	utils.CheckExclusive(ctx, utils.IPCDisabledFlag, utils.IPCPathFlag)
 	switch {
 	case ctx.GlobalBool(utils.IPCDisabledFlag.Name):
 		cfg.IPCPath = ""
@@ -453,44 +453,6 @@ func setAPIConfig(ctx *cli.Context) {
 func setNodeUserIdent(ctx *cli.Context, cfg *node.Config) {
 	if identity := ctx.GlobalString(utils.IdentityFlag.Name); len(identity) > 0 {
 		cfg.UserIdent = identity
-	}
-}
-
-// CheckExclusive verifies that only a single instance of the provided flags was
-// set by the user. Each flag might optionally be followed by a string type to
-// specialize it further.
-func CheckExclusive(ctx *cli.Context, args ...interface{}) {
-	set := make([]string, 0, 1)
-	for i := 0; i < len(args); i++ {
-		// Make sure the next argument is a flag and skip if not set
-		flag, ok := args[i].(cli.Flag)
-		if !ok {
-			panic(fmt.Sprintf("invalid argument, not cli.Flag type: %T", args[i]))
-		}
-		// Check if next arg extends current and expand its name if so
-		name := flag.GetName()
-
-		if i+1 < len(args) {
-			switch option := args[i+1].(type) {
-			case string:
-				// Extended flag, expand the name and shift the arguments
-				if ctx.GlobalString(flag.GetName()) == option {
-					name += "=" + option
-				}
-				i++
-
-			case cli.Flag:
-			default:
-				panic(fmt.Sprintf("invalid argument, not cli.Flag or string extension: %T", args[i+1]))
-			}
-		}
-		// Mark the flag if it's set
-		if ctx.GlobalIsSet(flag.GetName()) {
-			set = append(set, "--"+name)
-		}
-	}
-	if len(set) > 1 {
-		log.Fatalf("Flags %v can't be used at the same time", strings.Join(set, ", "))
 	}
 }
 
