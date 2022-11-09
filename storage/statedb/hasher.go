09624324cc2c7d232bb7e4ad4138f74688f217a2
hqjang-pepper <pepper.jang@groundx.xyz>
2022-11-09 17:55:04 +0900

    use new encoder in hasher

--- storage/statedb/hasher.go
+++ storage/statedb/hasher.go
@@ -36,22 +36,12 @@ type KeccakState interface {
 	Read([]byte) (int, error)
 }
 
-type sliceBuffer []byte
-
-func (b *sliceBuffer) Write(data []byte) (n int, err error) {
-	*b = append(*b, data...)
-	return len(data), nil
-}
-
-func (b *sliceBuffer) Reset() {
-	*b = (*b)[:0]
-}
-
 // hasher is a type used for the trie Hash operation. A hasher has some
 // internal preallocated temp space
 type hasher struct {
 	sha      KeccakState
-	tmp      sliceBuffer
+	tmp      []byte
+	encBuf   rlp.EncoderBuffer
 	parallel bool
 }
 
@@ -59,8 +49,9 @@ type hasher struct {
 var hasherPool = sync.Pool{
 	New: func() interface{} {
 		return &hasher{
-			tmp: make(sliceBuffer, 0, 550), // cap is as large as a full fullNode.
-			sha: sha3.NewLegacyKeccak256().(KeccakState),
+			tmp:    make([]byte, 0, 550), // cap is as large as a full fullNode.
+			sha:    sha3.NewLegacyKeccak256().(KeccakState),
+			encBuf: rlp.NewEncoderBuffer(nil),
 		}
 	},
 }
@@ -166,30 +157,41 @@ func (h *hasher) hashFullNodeChildren(n *fullNode) (collapsed *fullNode, cached 
 // into compact form for RLP encoding.
 // If the rlp data is smaller than 32 bytes, `nil` is returned.
 func (h *hasher) shortnodeToHash(n *shortNode, force bool) node {
-	h.tmp.Reset()
-	if err := rlp.Encode(&h.tmp, n); err != nil {
-		panic("encode error: " + err.Error())
-	}
+	n.encode(h.encBuf)
+	encResultBytes := h.encodedBytes()
 
-	if len(h.tmp) < 32 && !force {
+	if len(encResultBytes) < 32 && !force {
 		return n // Nodes smaller than 32 bytes are stored inside their parent
 	}
-	return h.hashData(h.tmp)
+	return h.hashData(encResultBytes)
 }
 
 // shortnodeToHash is used to creates a hashNode from a set of hashNodes, (which
 // may contain nil values)
 func (h *hasher) fullnodeToHash(n *fullNode, force bool) node {
-	h.tmp.Reset()
-	// Generate the RLP encoding of the node
-	if err := n.EncodeRLP(&h.tmp); err != nil {
-		panic("encode error: " + err.Error())
-	}
+	n.encode(h.encBuf)
+	encResultBytes := h.encodedBytes()
 
-	if len(h.tmp) < 32 && !force {
+	if len(encResultBytes) < 32 && !force {
 		return n // Nodes smaller than 32 bytes are stored inside their parent
 	}
-	return h.hashData(h.tmp)
+	return h.hashData(encResultBytes)
+}
+
+// encodedBytes returns the result of the last encoding operation on h.encBuf.
+// This also resets the encoder buffer.
+//
+// All node encoding must be done like this:
+//
+//     node.encode(h.encBuf)
+//     enc := h.encodedBytes()
+//
+// This convention exists because node.encode can only be inlined/escape-analyzed when
+// called on a concrete receiver type.
+func (h *hasher) encodedBytes() []byte {
+	h.tmp = h.encBuf.AppendToBytes(h.tmp[:0])
+	h.encBuf.Reset(nil)
+	return h.tmp
 }
 
 // hashData hashes the provided data
