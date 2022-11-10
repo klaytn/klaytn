2a88399bf22b3fe6c6069bd99648a2e5661fd031
hqjang-pepper <pepper.jang@groundx.xyz>
2022-11-10 15:36:59 +0900

    cherry-pick from previous PR(Optimize deriveShaOrig)

--- blockchain/types/derive_sha.go
+++ blockchain/types/derive_sha.go
@@ -21,8 +21,6 @@
 package types
 
 import (
-	"bytes"
-
 	"github.com/klaytn/klaytn/common"
 	"github.com/klaytn/klaytn/crypto/sha3"
 	"github.com/klaytn/klaytn/rlp"
@@ -125,26 +123,23 @@ type DeriveShaOrig struct{}
 
 func (d DeriveShaOrig) DeriveSha(list DerivableList, hasher TrieHasher) common.Hash {
 	hasher.Reset()
-	keybuf := new(bytes.Buffer)
+	var buf []byte
 
 	// StackTrie requires values to be inserted in increasing
 	// hash order, which is not the order that `list` provides
 	// hashes in. This insertion sequence ensures that the
 	// order is correct.
 	for i := 1; i < list.Len() && i <= 0x7f; i++ {
-		keybuf.Reset()
-		rlp.Encode(keybuf, uint(i))
-		hasher.Update(keybuf.Bytes(), list.GetRlp(i))
+		buf = rlp.AppendUint64(buf[:0], uint64(i))
+		hasher.Update(buf, list.GetRlp(i))
 	}
 	if list.Len() > 0 {
-		keybuf.Reset()
-		rlp.Encode(keybuf, uint(0))
-		hasher.Update(keybuf.Bytes(), list.GetRlp(0))
+		buf = rlp.AppendUint64(buf[:0], 0)
+		hasher.Update(buf, list.GetRlp(0))
 	}
 	for i := 0x80; i < list.Len(); i++ {
-		keybuf.Reset()
-		rlp.Encode(keybuf, uint(i))
-		hasher.Update(keybuf.Bytes(), list.GetRlp(i))
+		buf = rlp.AppendUint64(buf[:0], uint64(i))
+		hasher.Update(buf, list.GetRlp(i))
 	}
 	return hasher.Hash()
 }
