a18b9bec75a09f162bbe3542dad1bd3af8f57af8
Chihyun Song <ian0371@gmail.com>
2022-11-18 16:11:45 +0900

    Update ToBlock to load baseFee from genesis.json

--- blockchain/genesis.go
+++ blockchain/genesis.go
@@ -331,8 +331,14 @@ func (g *Genesis) ToBlock(baseStateRoot common.Hash, db database.DBManager) *typ
 		head.BlockScore = params.GenesisBlockScore
 	}
 	// TODO-klaytn should set genesis block's base fee
-	if g.Config != nil && g.Config.IsMagmaForkEnabled(common.Big0) {
-		head.BaseFee = new(big.Int).SetUint64(params.DefaultLowerBoundBaseFee)
+	if g.Config.IsMagmaForkEnabled(common.Big0) {
+		if g.Config != nil &&
+			g.Config.Governance != nil &&
+			g.Config.Governance.KIP71 != nil {
+			head.BaseFee = new(big.Int).SetUint64(g.Config.Governance.KIP71.LowerBoundBaseFee)
+		} else {
+			head.BaseFee = new(big.Int).SetUint64(params.DefaultLowerBoundBaseFee)
+		}
 	}
 	stateDB.Commit(false)
 	stateDB.Database().TrieDB().Commit(root, true, g.Number)
