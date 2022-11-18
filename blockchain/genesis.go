48be79cb00d0cb807189dd8e4824b5049a755758
Chihyun Song <ian0371@gmail.com>
2022-11-18 16:48:16 +0900

    Remove TODO

--- blockchain/genesis.go
+++ blockchain/genesis.go
@@ -330,7 +330,7 @@ func (g *Genesis) ToBlock(baseStateRoot common.Hash, db database.DBManager) *typ
 	if g.BlockScore == nil {
 		head.BlockScore = params.GenesisBlockScore
 	}
-	// TODO-klaytn should set genesis block's base fee
+
 	if g.Config.IsMagmaForkEnabled(common.Big0) {
 		if g.Config != nil &&
 			g.Config.Governance != nil &&
