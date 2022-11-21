1e881ea23f5a61202aef1b0b59dfdbf202de9119
Chihyun Song <ian0371@gmail.com>
2022-11-21 10:35:28 +0900

    Add GetRewards API

--- governance/api.go
+++ governance/api.go
@@ -96,6 +96,23 @@ func (api *GovernanceKlayAPI) GasPriceAt(num *rpc.BlockNumber) (*hexutil.Big, er
 	}
 }
 
+// or returns gas price of txpool if the block is pending block.
+func (api *GovernanceKlayAPI) GetRewards(num *rpc.BlockNumber) (*reward.RewardSpec, error) {
+	if num == nil || *num == rpc.LatestBlockNumber {
+		header := api.chain.CurrentHeader()
+		config := api.chain.Config()
+		return reward.GetBlockReward(header, config)
+	}
+
+	header := api.chain.GetHeaderByNumber(num.Uint64())
+	pset, err := api.governance.ParamsAt(num.Uint64())
+	if err != nil {
+		return nil, err
+	}
+	config := pset.ToChainConfig()
+	return reward.GetBlockReward(header, config)
+}
+
 // Vote injects a new vote for governance targets such as unitprice and governingnode.
 func (api *PublicGovernanceAPI) Vote(key string, val interface{}) (string, error) {
 	gMode := api.governance.Params().GovernanceModeInt()
