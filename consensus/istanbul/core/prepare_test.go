51da7b28b5a3f9d53f6103e7f11c162d7ef807ae
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2022-11-21 17:27:07 +0900

    [Consensus] Added a tc for the `Subject` struct cmp

--- consensus/istanbul/core/prepare_test.go
+++ consensus/istanbul/core/prepare_test.go
@@ -2,8 +2,10 @@ package core
 
 import (
 	"math/big"
+	"math/rand"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/klaytn/klaytn/blockchain/types"
@@ -12,6 +14,7 @@ import (
 	mock_istanbul "github.com/klaytn/klaytn/consensus/istanbul/mocks"
 	"github.com/klaytn/klaytn/fork"
 	"github.com/klaytn/klaytn/params"
+	"github.com/stretchr/testify/assert"
 	"gotest.tools/assert/cmp"
 )
 
@@ -118,4 +121,59 @@ func BenchmarkMsgCmp(b *testing.B) {
 			s1.Equal(&s2)
 		}
 	})
+}
+
+func TestSubjectCmp(t *testing.T) {
+	genRandomHash := func(n int) common.Hash {
+		b := make([]byte, n)
+		_, err := rand.Read(b)
+		assert.Nil(t, err)
+		return common.BytesToHash(b)
+	}
+	genRandomInt := func(min, max int) int64 {
+		return int64(rand.Intn(max-min) + min)
+	}
+	genSubject := func(min, max int) *istanbul.Subject {
+		round, seq := big.NewInt(genRandomInt(min, max)), big.NewInt(genRandomInt(min, max))
+		digest, prevHash := genRandomHash(max), genRandomHash(max)
+		return &istanbul.Subject{
+			View: &istanbul.View{
+				Round:    round,
+				Sequence: seq,
+			},
+			Digest:   digest,
+			PrevHash: prevHash,
+		}
+	}
+	copySubject := func(s *istanbul.Subject) *istanbul.Subject {
+		r := new(istanbul.Subject)
+		v := new(istanbul.View)
+		r.Digest = s.Digest
+		r.PrevHash = s.PrevHash
+		v.Round = new(big.Int).SetUint64(s.View.Round.Uint64())
+		v.Sequence = new(big.Int).SetUint64(s.View.Sequence.Uint64())
+		r.View = v
+		return r
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	min, max, n := 1, 9999, 10000
+	var identity bool
+	var s1, s2 *istanbul.Subject
+	for i := 0; i < n; i++ {
+		s1 = genSubject(min, max)
+		if rand.Intn(2) == 0 {
+			identity = true
+			s2 = copySubject(s1)
+		} else {
+			identity = false
+			s2 = genSubject(max+1, max*2)
+		}
+		e := s1.Equal(s2)
+		if identity {
+			assert.Equal(t, e, true)
+		} else {
+			assert.Equal(t, e, false)
+		}
+	}
 }
