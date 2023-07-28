60a181cfa660b9433d022b2e962ba4b207283ffc
ollie.j <ollie.j@krustuniverse.com>
2023-07-28 16:50:50 +0900

    Fix TestAccountListEmpty

--- cmd/utils/nodecmd/accountcmd_test.go
+++ cmd/utils/nodecmd/accountcmd_test.go
@@ -46,7 +46,8 @@ func tmpDatadirWithKeystore(t *testing.T) string {
 }
 
 func TestAccountListEmpty(t *testing.T) {
-	klay := runKlay(t, "klay-test", "account", "list")
+	datadir := tmpdir(t)
+	klay := runKlay(t, "klay-test", "account", "list", "--datadir", datadir)
 	klay.ExpectExit()
 }
 
