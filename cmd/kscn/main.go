8e9b1bfd419a153f525aca4e95db4cd878adcec5
kjeom <jack.jin@klaytn.foundation>
2023-08-08 16:06:37 +0900

    delete help printer

--- cmd/kscn/main.go
+++ cmd/kscn/main.go
@@ -41,7 +41,6 @@ var (
 )
 
 func init() {
-	utils.InitHelper()
 	// Initialize the CLI app and start kcn
 	app.Action = nodecmd.RunKlaytnNode
 	app.HideVersion = true // we have a command to print the version
@@ -73,9 +72,6 @@ func init() {
 	sort.Sort(cli.CommandsByName(app.Commands))
 
 	app.Flags = utils.KscnAppFlags()
-
-	cli.AppHelpTemplate = utils.GlobalAppHelpTemplate
-	cli.HelpPrinter = utils.NewHelpPrinter(utils.CategorizeFlags(app.Flags))
 
 	app.CommandNotFound = nodecmd.CommandNotExist
 	app.OnUsageError = nodecmd.OnUsageError
