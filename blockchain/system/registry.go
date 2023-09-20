b67f84294d0f3e1fc912e8f804abad053d3dfa6b
ollie.j <ollie.j@klaytn.foundation>
2023-09-20 19:01:12 +0900

    Add module blockchain/system and Registry accessors

---
+++ blockchain/system/registry.go
@@ -0,0 +1,49 @@
+// Copyright 2023 The klaytn Authors
+// This file is part of the klaytn library.
+//
+// The klaytn library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The klaytn library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
+
+package system
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/klaytn/klaytn/accounts/abi/bind"
+	"github.com/klaytn/klaytn/blockchain/state"
+	"github.com/klaytn/klaytn/common"
+	contracts "github.com/klaytn/klaytn/contracts/system_contracts"
+)
+
+func InstallRegistry(state *state.StateDB) error {
+	return state.SetCode(RegistryAddr, RegistryCode)
+}
+
+func ReadRegistryActiveAddr(backend bind.ContractCaller, name string, num *big.Int) (common.Address, error) {
+	caller, err := contracts.NewIRegistryCaller(RegistryAddr, backend)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	code, err := backend.CodeAt(context.Background(), RegistryAddr, nil)
+	if err != nil {
+		return common.Address{}, err
+	} else if code == nil {
+		return common.Address{}, ErrRegistryNotInstalled
+	}
+
+	opts := &bind.CallOpts{BlockNumber: num}
+	addr, err := caller.GetActiveContract(opts, name)
+	return addr, err
+}
---
+++ blockchain/system/registry_test.go
@@ -0,0 +1,54 @@
+package system
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/klaytn/klaytn/accounts/abi/bind"
+	"github.com/klaytn/klaytn/accounts/abi/bind/backends"
+	"github.com/klaytn/klaytn/blockchain"
+	"github.com/klaytn/klaytn/common"
+	contracts "github.com/klaytn/klaytn/contracts/system_contracts"
+	"github.com/klaytn/klaytn/crypto"
+	"github.com/klaytn/klaytn/log"
+	"github.com/klaytn/klaytn/params"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSystemRegistry(t *testing.T) {
+	log.EnableLogForTest(log.LvlCrit, log.LvlInfo)
+	var (
+		senderKey, _ = crypto.GenerateKey()
+		sender       = bind.NewKeyedTransactor(senderKey)
+		senderAddr   = sender.From
+
+		alloc = blockchain.GenesisAlloc{
+			senderAddr: {
+				Balance: big.NewInt(params.KLAY),
+			},
+			RegistryAddr: {
+				Code:    RegistryMockCode,
+				Balance: common.Big0,
+			},
+		}
+		backend = backends.NewSimulatedBackend(alloc)
+
+		recordName = "AcmeContract"
+		recordAddr = common.HexToAddress("0xaaaa")
+	)
+
+	// Without a record
+	addr, err := ReadRegistryActiveAddr(backend, recordName, common.Big0)
+	assert.Nil(t, err)
+	assert.Equal(t, common.Address{}, addr)
+
+	// Register a record
+	contract, _ := contracts.NewRegistryMockTransactor(RegistryAddr, backend)
+	contract.Register(sender, recordName, recordAddr)
+	backend.Commit()
+
+	// With the record
+	addr, err = ReadRegistryActiveAddr(backend, recordName, common.Big1)
+	assert.Nil(t, err)
+	assert.Equal(t, recordAddr, addr)
+}
