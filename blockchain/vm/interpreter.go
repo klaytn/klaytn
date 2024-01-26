155b7c84a0961d0aeaf0d5dd344d5de69a31651c
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2024-01-26 11:04:23 +0900

    [] Reverted

--- blockchain/vm/interpreter.go
+++ blockchain/vm/interpreter.go
@@ -271,8 +271,8 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte) (ret []byte, err
 		// Dynamic portion of gas
 		// consume the gas and return an error if not enough gas is available.
 		// cost is explicitly set so that the capture state defer method can get the proper cost
-		var dynamicCost uint64 = 0
 		if operation.dynamicGas != nil {
+			var dynamicCost uint64
 			dynamicCost, err = operation.dynamicGas(in.evm, contract, stack, mem, memorySize)
 			cost += dynamicCost // total cost, for debug tracing
 			if err != nil || !contract.UseGas(dynamicCost) {
