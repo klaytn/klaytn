93590812f76b1b029b281a09ec6d057b19d585ff
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2024-02-14 10:25:36 +0900

    [API] Aliend with the gas cost output

--- blockchain/vm/interpreter.go
+++ blockchain/vm/interpreter.go
@@ -185,10 +185,12 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte) (ret []byte, err
 		// to be uint256. Practically much less so feasible.
 		pc   = uint64(0) // program counter
 		cost uint64
+		cc   uint64
 		// copies used by tracer
-		pcCopy              uint64              // needed for the deferred Tracer
-		gasCopy             uint64              // for Tracer to log gas remaining before execution
-		logged              bool                // deferred Tracer should ignore already logged steps
+		pcCopy              uint64 // needed for the deferred Tracer
+		gasCopy             uint64 // for Tracer to log gas remaining before execution
+		logged              bool   // deferred Tracer should ignore already logged steps
+		ccCostCopy          uint64
 		res                 []byte              // result of the opcode execution function
 		allocatedMemorySize = uint64(mem.Len()) // Currently allocated memory size
 
@@ -200,14 +202,10 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte) (ret []byte, err
 	if in.cfg.Debug {
 		defer func() {
 			if err != nil {
-				cc := uint64(0)
-				if in.cfg.JumpTable[op] != nil {
-					cc = in.cfg.JumpTable[op].computationCost
-				}
 				if !logged {
-					in.cfg.Tracer.CaptureState(in.evm, pcCopy, op, gasCopy, cost, cc, callContext, in.evm.depth, err)
+					in.cfg.Tracer.CaptureState(in.evm, pcCopy, op, gasCopy, cost, ccCostCopy, cc, callContext, in.evm.depth, err)
 				} else {
-					in.cfg.Tracer.CaptureFault(in.evm, pcCopy, op, gasCopy, cost, cc, callContext, in.evm.depth, err)
+					in.cfg.Tracer.CaptureFault(in.evm, pcCopy, op, gasCopy, cost, ccCostCopy, cc, callContext, in.evm.depth, err)
 				}
 			}
 		}()
@@ -223,7 +221,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte) (ret []byte, err
 		}
 		if in.cfg.Debug {
 			// Capture pre-execution values for tracing.
-			logged, pcCopy, gasCopy = false, pc, contract.Gas
+			logged, pcCopy, gasCopy, ccCostCopy = false, pc, contract.Gas, in.evm.Config.ComputationCostLimit-in.evm.opcodeComputationCostSum
 		}
 
 		// Get the operation from the jump table and validate the stack to ensure there are
@@ -251,6 +249,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte) (ret []byte, err
 		if in.evm.opcodeComputationCostSum > in.evm.Config.ComputationCostLimit {
 			return nil, ErrOpcodeComputationCostLimitReached
 		}
+		cc = operation.computationCost
 		var memorySize uint64
 		var extraSize uint64
 		// calculate the new memory size and expand the memory to fit
@@ -288,7 +287,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte) (ret []byte, err
 		}
 
 		if in.cfg.Debug {
-			in.cfg.Tracer.CaptureState(in.evm, pc, op, gasCopy, cost, operation.computationCost, callContext, in.evm.depth, err)
+			in.cfg.Tracer.CaptureState(in.evm, pc, op, gasCopy, cost, ccCostCopy, cc, callContext, in.evm.depth, err)
 			logged = true
 		}
 
--- blockchain/vm/logger.go
+++ blockchain/vm/logger.go
@@ -113,8 +113,8 @@ type Tracer interface {
 	CaptureEnter(typ OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int)
 	CaptureExit(output []byte, gasUsed uint64, err error)
 	// Opcode level
-	CaptureState(env *EVM, pc uint64, op OpCode, gas, cost, cc uint64, scope *ScopeContext, depth int, err error)
-	CaptureFault(env *EVM, pc uint64, op OpCode, gas, cost, cc uint64, scope *ScopeContext, depth int, err error)
+	CaptureState(env *EVM, pc uint64, op OpCode, gas, cost, cc, ccUnit uint64, scope *ScopeContext, depth int, err error)
+	CaptureFault(env *EVM, pc uint64, op OpCode, gas, cost, cc, ccUnit uint64, scope *ScopeContext, depth int, err error)
 }
 
 // StructLogger is an EVM state logger and implements Tracer.
@@ -149,7 +149,7 @@ func (l *StructLogger) CaptureStart(env *EVM, from common.Address, to common.Add
 // CaptureState logs a new structured log message and pushes it out to the environment
 //
 // CaptureState also tracks SSTORE ops to track dirty values.
-func (l *StructLogger) CaptureState(env *EVM, pc uint64, op OpCode, gas, cost, opcodeComputationCost uint64, scope *ScopeContext, depth int, err error) {
+func (l *StructLogger) CaptureState(env *EVM, pc uint64, op OpCode, gas, cost, cc, ccUnit uint64, scope *ScopeContext, depth int, err error) {
 	memory := scope.Memory
 	stack := scope.Stack
 	contract := scope.Contract
@@ -193,15 +193,14 @@ func (l *StructLogger) CaptureState(env *EVM, pc uint64, op OpCode, gas, cost, o
 		storage = l.changedValues[contract.Address()].Copy()
 	}
 	// create a new snapshot of the EVM.
-	remainingComputationCost := env.Config.ComputationCostLimit - env.opcodeComputationCostSum
-	log := StructLog{pc, op, gas, cost, mem, memory.Len(), stck, storage, depth, env.StateDB.GetRefund(), remainingComputationCost, opcodeComputationCost, err}
+	log := StructLog{pc, op, gas, cost, mem, memory.Len(), stck, storage, depth, env.StateDB.GetRefund(), cc, ccUnit, err}
 
 	l.logs = append(l.logs, log)
 }
 
 // CaptureFault implements the Tracer interface to trace an execution fault
 // while running an opcode.
-func (l *StructLogger) CaptureFault(env *EVM, pc uint64, op OpCode, gas, cost, cc uint64, scope *ScopeContext, depth int, err error) {
+func (l *StructLogger) CaptureFault(env *EVM, pc uint64, op OpCode, gas, cost, cc, ccUnit uint64, scope *ScopeContext, depth int, err error) {
 }
 
 // CaptureEnd is called after the call finishes to finalize the tracing.
